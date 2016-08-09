44ee0eed5ad5170a8fb5cd6877797c89867c90f1
Nicolas James <nicolas.james@gmail.com>
2016-08-09 20:35:09 +0200

    added list of packages to remove

--- InstallPackages.sh
+++ InstallPackages.sh
@@ -6,11 +6,7 @@ BASEDIR=$(dirname ${SCRIPT_PATH})
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# Ask user for installing group of packages?
-#while read -r line; do sudo apt-get install "$line"; done < ./packages.list
-
-# Install everything without asking
-xargs sudo apt-get -y install < ./packages.list
-
-sudo apt-get -y remove file-roller
+xargs sudo apt-get -y remove < ./packagesToRemove.list
 sudo apt-get -y autoremove
+
+xargs sudo apt-get -y install < ./packagesToInstall.list
