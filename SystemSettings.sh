420a00db51601eb653ea7da4a89acbfd1ffc36ce
Nicolas James <nicolas.james@gmail.com>
2016-08-20 13:41:47 +0200

    fixed SystemSettings.sh

--- SystemSettings.sh
+++ SystemSettings.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $(id -u) -ne 0 ]; then
+  echo "Please run with root privileges"
+  exit
+fi
+
 . ./common.sh
 
 SCRIPT_PATH=$(readlink -f "$0")
@@ -8,28 +13,28 @@ LIGHTDM_GREETER_OPENBOX_BADGE_FILE_NAME=openbox_badge-symbolic#1.svg
 
 disable_apport(){
   echo "Disabling apport ..."
-  sudo sed -i '/^enabled=/s/.*/enabled=0/' /etc/default/apport
+  sed -i '/^enabled=/s/.*/enabled=0/' /etc/default/apport
 }
 
 add_lightdm_greeter_badges(){
   echo "Adding lighdm greeter badges ..."
   # Greeter badge for Openbox
   cd ${BASEDIR}/lightdm-greeter-badge
-  sudo cp ${LIGHTDM_GREETER_OPENBOX_BADGE_FILE_NAME} /usr/share/icons/hicolor/scalable/places/openbox_badge-symbolic.svg
-  sudo gtk-update-icon-cache /usr/share/icons/hicolor
+  cp ${LIGHTDM_GREETER_OPENBOX_BADGE_FILE_NAME} /usr/share/icons/hicolor/scalable/places/openbox_badge-symbolic.svg
+  gtk-update-icon-cache /usr/share/icons/hicolor
 }
 
 configure_alternatives(){
   echo "Setting mate-terminal as default x-terminal-emulator ..."
-  sudo update_alternatives set x-terminal-emulator /usr/bin/mate-terminal.wrapper
+  update_alternatives set x-terminal-emulator /usr/bin/mate-terminal.wrapper
 }
 
 copy_themes(){
   echo "Copying themes ..."
   cd ${BASEDIR}/themes
   tar xzf Erthe-njames.tar.gz
-  sudo cp -R Erthe-njames /usr/share/themes
-  sudo chmod -R 755 /usr/share/themes
+  cp -R Erthe-njames /usr/share/themes
+  chmod -R 755 /usr/share/themes/Erthe-njames
   rm -fr Erthe-njames
 }
 
@@ -39,20 +44,20 @@ configure_gtk(){
 
   # GTK+ 2.0
   if [ -f /etc/gtk-2.0/gtkrc  ]; then
-    sudo sed -i '/^gtk-theme-name/s/.*/gtk-theme-name=\"Greybird\"/' /etc/gtk-2.0/gtkrc
-    sudo sed -i '/^gtk-icon-theme-name/s/.*/gtk-icon-theme-name=\"Faenza-Dark\"/' /etc/gtk-2.0/gtkrc
+    sed -i '/^gtk-theme-name/s/.*/gtk-theme-name=\"Greybird\"/' /etc/gtk-2.0/gtkrc
+    sed -i '/^gtk-icon-theme-name/s/.*/gtk-icon-theme-name=\"Faenza-Dark\"/' /etc/gtk-2.0/gtkrc
   else
-    sudo cp system.gtkrc-2.0 /etc/gtk-2.0/gtkrc
-    sudo chmod 755 /etc/gtk-2.0/gtkrc
+    cp system.gtkrc-2.0 /etc/gtk-2.0/gtkrc
+    chmod 755 /etc/gtk-2.0/gtkrc
   fi
 
   # GTK+ 3.0
   if [ -f /etc/gtk-3.0/settings.ini ]; then
-    sudo sed -i '/^gtk-theme-name/s/.*/gtk-theme-name=Greybird/' /etc/gtk-3.0/settings.ini
-    sudo sed -i '/^gtk-icon-theme-name/s/.*/gtk-icon-theme-name=\"Faenza-Dark\"/' /etc/gtk-3.0/settings.ini
+    sed -i '/^gtk-theme-name/s/.*/gtk-theme-name=Greybird/' /etc/gtk-3.0/settings.ini
+    sed -i '/^gtk-icon-theme-name/s/.*/gtk-icon-theme-name=\"Faenza-Dark\"/' /etc/gtk-3.0/settings.ini
   else
-    sudo cp system.gtkrc-3.0 /etc/gtk-3.0/settings.ini
-    sudo chmod 755 /etc/gtk-3.0/settings.ini
+    cp system.gtkrc-3.0 /etc/gtk-3.0/settings.ini
+    chmod 755 /etc/gtk-3.0/settings.ini
   fi
 
   # Disable the scrollbar overlay introduced in GTK+ 3.15
@@ -66,17 +71,17 @@ configure_gtk(){
 configure_grub(){
   echo "Configuring grub ..."
   # Remove hidden timeout 0 => show grub
-  sudo sed -i '/^GRUB_HIDDEN_TIMEOUT/s/.*/#GRUB_HIDDEN_TIMEOUT=0/' /etc/default/grub
+  sed -i '/^GRUB_HIDDEN_TIMEOUT/s/.*/#GRUB_HIDDEN_TIMEOUT=0/' /etc/default/grub
   # Remove boot option "quiet" and "splash"
-  sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/.*/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
-  sudo update-grub
+  sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/.*/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
+  update-grub
 }
 
 configure_bash_for_root(){
   echo "Configuring bash for root ..."
   cd ${BASEDIR}/bash
-  sudo renameFileForBackup /root/.bashrc
-  sudo cp bashrc /root/.bashrc
+  renameFileForBackup /root/.bashrc
+  cp bashrc /root/.bashrc
 }
 
 disable_apport
