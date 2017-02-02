06ceb098b84e65411ef23d2313d688a096621328
Nicolas James <nicolas.james@gmail.com>
2017-02-02 22:48:49 +0100

    updated MacBookAir.sh for Yakkety Yak

--- Desktop/MacBookAir7,1/16.10_YakketyYak/MacBookAir.sh
+++ Desktop/MacBookAir7,1/16.10_YakketyYak/MacBookAir.sh
@@ -3,27 +3,20 @@
 . ../../Common/common.sh
 
 retrieve_mac_book_air_product_name(){
-  printSectionHeading "Retrieving MacBook Air product name"
+  printSectionHeading "Retrieving MacBook Air product name ..."
   sudo dmidecode -s system-product-name
   printSectionEnding
 }
 
 configure_apple_hid(){
-  printSectionHeading "Configuring apple hid (use fn key for special functions keys)"
+  printSectionHeading "Configuring apple hid (use fn key for special functions keys) ..."
   echo options hid_apple fnmode=2 | sudo tee -a /etc/modprobe.d/hid_apple.conf
   sudo update-initramfs -u -k all
-  printSectionEnding
-}	
-
-# Trackpad is too sentitive: a tap is issued after scrolling
-configure_trackpad(){
-  printSectionHeading "Configuring trackpad sensitivity"
-  echo "synclient FingerLow=110 FingerHigh=120" | tee -a ~/.config/openbox/autostart
   printSectionEnding
 }
 
 configure_xmodmap(){
-  printSectionHeading "Configuring xmodmap (.xmodmaprc includes fix for backtick and tilde keys)"
+  printSectionHeading "Configuring xmodmap (.xmodmaprc includes fix for backtick and tilde keys) ..."
   cd ${BASEDIR}
   if [ -f ~/.xmodmaprc ]; then
     renameFileForBackup ~/.xmodmaprc
@@ -35,14 +28,14 @@ configure_xmodmap(){
 }	
 
 tune_power_save_functions(){
-  printSectionHeading "Tuning power save functions"
+  printSectionHeading "Tuning power save functions ..."
   sudo apt-get install -y powertop
   sudo apt-get install -y tlp tlp-rdw
   printSectionEnding
 }	
 
 install_facetimehd(){
-  printSectionHeading "Installing FacetimeHD"
+  printSectionHeading "Installing FacetimeHD ..."
   cd ${BASEDIR}
   tar xjf cpio-2.12.tar.bz2
   cd cpio-2.12
@@ -67,7 +60,6 @@ install_facetimehd(){
 
 retrieve_mac_book_air_product_name 2>&1 | tee -a StdOutErr.log
 configure_apple_hid 2>&1 | tee -a StdOutErr.log
-configure_trackpad 2>&1 | tee -a StdOutErr.log
 configure_xmodmap 2>&1 | tee -a StdOutErr.log
 tune_power_save_functions 2>&1 | tee -a StdOutErr.log
 
