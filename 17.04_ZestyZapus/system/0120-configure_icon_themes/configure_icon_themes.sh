72c508efff982cc2f1c0adaf5226cbb6f927b08b
Nicolas James <nicolas.james@gmail.com>
2018-01-09 20:23:12 +0100

    Fix for synaptic icon was not working

--- 17.04_ZestyZapus/system/0120-configure_icon_themes/configure_icon_themes.sh
+++ 17.04_ZestyZapus/system/0120-configure_icon_themes/configure_icon_themes.sh
@@ -14,16 +14,17 @@ install_customized_faenza(){
 	rm -fr ./faenza-vanilla-icon-theme/Faenza/status
 	cp -R ./faenza-vanilla-icon-theme/Faenza-Dark/status ./faenza-vanilla-icon-theme/Faenza/status
 	
-	echo "synaptic persists to use its 16x16 icon => dirty fix: replace the 16x16 by the 32x32 ..."
-	renameFileForBackup ./faenza-vanilla-icon-theme/Faenza/apps/16/synaptic.png
-	ln -s ./faenza-vanilla-icon-theme/Faenza/apps/32/synaptic.png ./faenza-vanilla-icon-theme/Faenza/apps/16/synaptic.png
-	
 	echo "Installing our customized Faenza ... "
 	sed -i "/^Name=/s/.*/Name=Faenza-njames/" ./faenza-vanilla-icon-theme/Faenza/index.theme
 	ESCAPED_COMMENT=$(escape_sed_pattern "Comment=Icon theme project downloaded from https://github.com/Kazhnuz/faenza-vanilla-icon-theme and modified by njames")
 	sed -i "/^Comment=/s/.*/${ESCAPED_COMMENT}/" ./faenza-vanilla-icon-theme/Faenza/index.theme
 	mv ./faenza-vanilla-icon-theme/Faenza ./faenza-vanilla-icon-theme/Faenza-njames
 	cp -R ./faenza-vanilla-icon-theme/Faenza-njames /usr/share/icons
+	
+	echo "synaptic persists to use its 16x16 icon => dirty fix: replace the 16x16 by the 32x32 ..."
+	cd /usr/share/icons/Faenza-njames/apps/16
+	renameFileForBackup ./synaptic.png
+	ln -s ../32/synaptic.png ./synaptic.png
 	
 	echo "Updating icon caches ..."
 	update-icon-caches /usr/share/icons
