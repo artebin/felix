3b16ff11960aa964251263ed166a0ef52d1b6883
Nicolas James <nicolas.james@gmail.com>
2018-01-19 16:31:45 +0100

    fix: do not change xdg autostart + gnome-keyring should be installed

--- 17.04_ZestyZapus/system/0210-disable_unwanted_xdg_autostart/disable_unwanted_xdg_autostart.sh
+++ 17.04_ZestyZapus/system/0210-disable_unwanted_xdg_autostart/disable_unwanted_xdg_autostart.sh
@@ -9,34 +9,41 @@ force_numlockx(){
 	
 	echo 'Disabling unwanted xdg autostart ...'
 	
-	#at-spi-dbus-bus.desktop
-	#blueman.desktop
-	backup_file rename /etc/xdg/autostart/gnome-keyring-pkcs11.desktop
-	backup_file rename /etc/xdg/autostart/gnome-keyring-secrets.desktop
-	backup_file rename /etc/xdg/autostart/gnome-keyring-ssh.desktop
-	backup_file rename /etc/xdg/autostart/gnome-software-service.desktop
-	backup_file rename /etc/xdg/autostart/gnome-user-share-obexpush.desktop
-	backup_file rename /etc/xdg/autostart/gnome-user-share-webdav.desktop
-	backup_file rename /etc/xdg/autostart/gsettings-data-convert.desktop
-	backup_file rename /etc/xdg/autostart/indicator-application.desktop
-	backup_file rename /etc/xdg/autostart/indicator-messages.desktop
-	#/etc/xdg/autostart/light-locker.desktop
-	#/etc/xdg/autostart/nm-applet.desktop
-	#/etc/xdg/autostart/onboard-autostart.desktop
-	#/etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop
-	#/etc/xdg/autostart/print-applet.desktop
-	#/etc/xdg/autostart/pulseaudio.desktop
-	backup_file rename /etc/xdg/autostart/unity-fallback-mount-helper.desktop
-	backup_file rename /etc/xdg/autostart/unity-settings-daemon.desktop
-	backup_file rename /etc/xdg/autostart/update-notifier.desktop
-	#/etc/xdg/autostart/user-dirs-update-gtk.desktop
-	backup_file rename /etc/xdg/autostart/xfce4-clipman-plugin-autostart.desktop
-	backup_file rename /etc/xdg/autostart/xfce4-notes-autostart.desktop
-	#/etc/xdg/autostart/xfce4-power-manager.desktop
-	#/etc/xdg/autostart/xfce4-volumed.desktop
-	#/etc/xdg/autostart/xfsettingsd.desktop
-	backup_file rename /etc/xdg/autostart/xscreensaver.desktop
-	backup_file rename /etc/xdg/autostart/zeitgeist-datahub.desktop
+	XDG_AUTOSTART_DESKTOP_FILE_ARRAY=(
+		#at-spi-dbus-bus.desktop
+		#blueman.desktop
+		#gnome-keyring-pkcs11.desktop
+		#gnome-keyring-secrets.desktop
+		#gnome-keyring-ssh.desktop
+		#gnome-software-service.desktop
+		#gnome-user-share-obexpush.desktop
+		#gnome-user-share-webdav.desktop
+		#gsettings-data-convert.desktop
+		#indicator-application.desktop
+		#indicator-messages.desktop
+		#light-locker.desktop
+		#nm-applet.desktop
+		#onboard-autostart.desktop
+		#polkit-gnome-authentication-agent-1.desktop
+		#print-applet.desktop
+		#pulseaudio.desktop
+		#unity-fallback-mount-helper.desktop
+		#unity-settings-daemon.desktop
+		#update-notifier.desktop
+		#user-dirs-update-gtk.desktop
+		#xfce4-clipman-plugin-autostart.desktop
+		#xfce4-notes-autostart.desktop
+		#xfce4-power-manager.desktop
+		#xfce4-volumed.desktop
+		#xfsettingsd.desktop
+		#xscreensaver.desktop
+		#zeitgeist-datahub.desktop
+	)
+	
+	for XDG_AUTOSTART_FILE_NAME in "${XDG_AUTOSTART_DESKTOP_FILE_ARRAY[@]}"; do
+		echo "Disabling autostart for ${XDG_AUTOSTART_FILE_NAME} ..."
+		#backup_file rename /etc/xdg/autostart/${XDG_AUTOSTART_FILE_NAME}
+	done
 }
 
 cd ${BASEDIR}
