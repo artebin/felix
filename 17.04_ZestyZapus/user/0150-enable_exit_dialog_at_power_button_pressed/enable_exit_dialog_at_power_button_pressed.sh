245ce5b581d66c24d75fe7511923e7af1be16c66
Nicolas James <nicolas.james@gmail.com>
2018-01-21 15:53:15 +0100

    recipe for "enable_exit_dialog_at_power_button_pressed" was not executed

--- 17.04_ZestyZapus/user/0150-enable_exit_dialog_at_power_button_pressed/enable_exit_dialog_at_power_button_pressed.sh
+++ 17.04_ZestyZapus/user/0150-enable_exit_dialog_at_power_button_pressed/enable_exit_dialog_at_power_button_pressed.sh
@@ -7,7 +7,9 @@ enable_exit_dialog_at_power_button_pressed(){
 	cd ${BASEDIR}
 	
 	echo 'Enabling exit dialog at power button pressed ...'
-	add_or_update_line_based_on_prefix "#HandlePowerKey=" "HandlePowerKey=ignore" "/etc/systemd/logind.conf"
+	cp /etc/systemd/logind.conf ./logind.conf
+	add_or_update_line_based_on_prefix "#HandlePowerKey=" "HandlePowerKey=ignore" ./logind.conf
+	sudo cp ./logind.conf /etc/systemd/logind.conf
 	# Configuration in openbox rc.xml file should already be done
 }
 
