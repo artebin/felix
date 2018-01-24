5f1ec6d14b29436b7a2f857139606c0c3183e418
Nicolas James <nicolas.james@gmail.com>
2018-01-24 10:38:41 +0100

    improved geany config with the geany version number + keybindings.conf

--- 17.04_ZestyZapus/user/0100-configure_geany/configure_geany.sh
+++ 17.04_ZestyZapus/user/0100-configure_geany/configure_geany.sh
@@ -7,13 +7,20 @@ configure_geany(){
 	cd ${BASEDIR}
 	
 	echo 'Configuring geany ...'
+	
+	# Backup pre-existing geany config directory if it exists
 	if [[ -f ~/.config/geany ]]; then
 		backup_file rename ~/.config/geany
 	fi
+	
+	# Create geany config directory if it does not exist
 	if [ ! -f ~/.config/geany ]; then
 		mkdir -p ~/.config/geany/filedefs
 	fi
-	cp ./filetypes.common ~/.config/geany/filedefs/filetypes.common
+	
+	# Copy geany config files
+	cp ./geany_1.29.filetypes.common ~/.config/geany/filedefs/filetypes.common
+	cp ./geany_1.29.keybindings.conf ~/.config/geany/keybindings.conf
 	
 	# Force geany to open files in new instance
 	if [ ! -f ~/.local/share/applications ]; then
