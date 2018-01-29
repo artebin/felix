aafec16e0eb6b7ea8c1bc555ada413c1699c61a6
Nicolas James <nicolas.james@gmail.com>
2018-01-29 22:48:22 +0100

    added doc

--- 17.04_ZestyZapus/user/0060-configure_xfce4_power_manager/configure_xfce4_power_manager.sh
+++ 17.04_ZestyZapus/user/0060-configure_xfce4_power_manager/configure_xfce4_power_manager.sh
@@ -6,6 +6,10 @@ check_shell
 configure_xfce4_power_manager(){
 	cd ${BASEDIR}
 	
+	# Xfce stores its config in ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+	# These files should not be changed while logged in xfce (they will be overwritten).
+	# Should use xfconf-query applying changes during xfce runtime.
+	
 	echo 'Configuring xfce4-power-manager ...'
 	xfconf-query --create -t int -c xfce4-power-manager -p /xfce4-power-manager/show-tray-icon -s 1
 	xfconf-query --create -t int -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-battery -s 1
