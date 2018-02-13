1abef02eae4ea48123203ce22c6b471a9e0b7a88
Nicolas James <nicolas.james@gmail.com>
2018-02-13 14:38:29 +0100

    minor addition

--- 17.04_ZestyZapus/user/0030-configure_openbox/dialog_command2.sh
+++ 17.04_ZestyZapus/user/0030-configure_openbox/dialog_command2.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
+# This will wait one second and then steal focus and make the Zenity dialog box always-on-top (aka. 'above').
 (sleep 1 && wmctrl -F -a "${1}" -b add,above) &
 DIALOG_RETURN_CODE=$(zenity --question --title="${1}" --text "${2}"; echo $?)
 if [ ${DIALOG_RETURN_CODE} = 0 ]; then
-  exec ${2}
+	exec ${2}
 fi
