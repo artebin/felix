4bd757028cfd27105d11c0f8a1a090968c7811d4
Nicolas James <nicolas.james@gmail.com>
2018-02-25 14:57:02 +0100

    added recipe configure_trackpad_mtrack

---
+++ 17.04_ZestyZapus/specifics/macbook_air_7_1/0031-configure_trackpad_mtrack/configure_trackpad_mtrack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ../../../common.sh
+check_shell
+
+configure_trackpad_mtrack{
+	cd ${BASEDIR}
+	
+	echo "Configuring trackpad mtrack ..."
+	
+	apt-get install -y xserver-xorg-input-mtrack
+	cp ./80-mtrack.conf /usr/share/X11/xorg.conf.d/
+}
+
+cd ${BASEDIR}
+configure_trackpad_mtrack 2>&1 | tee -a ./${CURRENT_SCRIPT_LOG_FILE_NAME}
