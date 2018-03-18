8dc1ff2c33fdd4482e68430e7a9e37dba3f57af8
Nicolas James <nicolas.james@gmail.com>
2018-03-18 18:07:26 +0100

    added system recipe 0260-s-configure_keyboard

---
+++ 17.04_ZestyZapus/system/0260-s-configure_keyboard/configure_keyboard.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source ../../common.sh
+check_shell
+exit_if_has_not_root_privileges
+
+configure_keyboard(){
+	cd ${BASEDIR}
+	
+	echo "Configuring keyboard ..."
+	
+	if [ ! -f /etc/default/keyboard ]; then
+		echo "Can not find /etc/default/keyboard"
+		exit 1
+	fi
+	
+	backup_file copy /etc/default/keyboard
+	
+	echo "XKBMODEL=\"${XKBMODEL}\""
+	add_or_update_line_based_on_prefix "XKBMODEL=" "XKBMODEL=\"${XKBMODEL}\"" /etc/default/keyboard
+	
+	echo "XKBLAYOUT=\"${XKBLAYOUT}\""
+	add_or_update_line_based_on_prefix "XKBLAYOUT=" "XKBLAYOUT=\"${XKBLAYOUT}\"" /etc/default/keyboard
+	
+	echo "XKBVARIANT=\"${XKBVARIANT}\""
+	add_or_update_line_based_on_prefix "XKBVARIANT=" "XKBVARIANT=\"${XKBVARIANT}\"" /etc/default/keyboard
+	
+	echo "XKBOPTIONS=\"${XKBOPTIONS}\""
+	add_or_update_line_based_on_prefix "XKBOPTIONS=" "XKBOPTIONS=\"${XKBOPTIONS}\"" /etc/default/keyboard
+}
+
+cd ${BASEDIR}
+configure_keyboard 2>&1 | tee -a ./${CURRENT_SCRIPT_LOG_FILE_NAME}
