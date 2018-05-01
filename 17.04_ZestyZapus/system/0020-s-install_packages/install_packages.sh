dbd2f5ccca29a877ad08bdfdfb8291a0a792b40b
Nicolas James <nicolas.james@gmail.com>
2018-05-01 15:01:12 +0200

    added comments in file format of packages.install.list, used for package category in the file

--- 17.04_ZestyZapus/system/0020-s-install_packages/install_packages.sh
+++ 17.04_ZestyZapus/system/0020-s-install_packages/install_packages.sh
@@ -8,7 +8,26 @@ process_package_install_list(){
 	cd ${BASEDIR}
 	
 	echo "Installing packages ..."
-	xargs apt-get -y install < ./packages.install.list
+	
+	APT_INPUT_FILE="./apt.pkg.list"
+	
+	if [ -f "${APT_INPUT_FILE}" ]; then
+		rm -f "${APT_INPUT_FILE}"
+	fi
+	while read LINE; do
+		INDEX_OF_COMMENT=`expr index "${LINE}" '#'`
+		if [ "${INDEX_OF_COMMENT}" -eq 0 ]; then
+			PACKAGE_LINE_LIST=${LINE}
+		else
+			SUBSTRING_LENGTH=`expr ${INDEX_OF_COMMENT}-1`
+			PACKAGE_LINE_LIST=${LINE:0:${SUBSTRING_LENGTH}}
+		fi
+		if [ ! -z "${PACKAGE_LINE_LIST}" ]; then
+			echo ${PACKAGE_LINE_LIST} >>"${APT_INPUT_FILE}"
+		fi
+	done < ./packages.install.list
+	
+	xargs apt-get -y install < "${APT_INPUT_FILE}"
 }
 
 cd ${BASEDIR}
