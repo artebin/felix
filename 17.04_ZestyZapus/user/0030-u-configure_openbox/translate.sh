df8b8547f841d7af592188837bdfb193c3a36abd
Nicolas James <nicolas.james@gmail.com>
2018-05-09 13:15:53 +0200

    minor improvements in translate.sh

--- 17.04_ZestyZapus/user/0030-u-configure_openbox/translate.sh
+++ 17.04_ZestyZapus/user/0030-u-configure_openbox/translate.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 SELECTED_TEXT=$(xsel -o)
+if [ -z "${SELECTED_TEXT}" ]; then
+	exit 0
+fi
+
 TRANSLATION=$(trans -b :fr "${SELECTED_TEXT}")
-notify-send --icon=info "${SELECTED_TEXT}" "${TRANSLATION}"
+NOTIFICATION_TEXT="${SELECTED_TEXT}\n\n<i>${TRANSLATION}</i>"
+
+# Desktop Notifications Specification <http://www.galago-project.org/specs/notification/0.9/x161.html>
+notify-send --icon=info "Translation" "${NOTIFICATION_TEXT}"
