390386a9fb4b6a01fda633cc52e9f70ff98df6f4
Nicolas James <nicolas.james@gmail.com>
2018-05-14 11:52:47 +0200

    minor change in extractColor.sh

--- UserScripts/extractColor.sh
+++ UserScripts/extractColor.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for i in {0..257}; do
-	printf "%-15s" "Color#${i}: "
-	printf "\e[48;5;${i}m    \e[0m"
+for ESCAPE_SEQUENCE_INDEX in {0..257}; do
+	printf "%-15s" "Color [ ${ESCAPE_SEQUENCE_INDEX} ]"
+	printf " [\e[48;5;${ESCAPE_SEQUENCE_INDEX}m%-15s\e[0m]" " "
+	printf " %-15s" "RGB [ ]"
 	echo
 done
 echo
