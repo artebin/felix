f6c2bc3144310245ebc669fb99c1a25635f93db7
Nicolas James <nicolas.james@gmail.com>
2018-06-03 23:19:37 +0200

    added caja scripts in recipe configure_mate_caja + added script diff_with

--- 18.04_BionicBeaver/user/0090-u-configure_mate_caja/configure_mate_caja.sh
+++ 18.04_BionicBeaver/user/0090-u-configure_mate_caja/configure_mate_caja.sh
@@ -7,8 +7,17 @@ configure_mate_caja(){
 	cd ${BASEDIR}
 	
 	echo "Configuring mate-caja ..."
+	
+	echo "Importing caja configuration in dconf ..."
 	dconf load /org/mate/caja/ < ./org.mate.caja.dump
 	dconf load /org/mate/desktop/ < ./org.mate.desktop.dump
+	
+	echo "Adding caja scripts ..."
+	if [ -d ~/.config/caja/scripts ]; then
+	backup_file rename ~/.config/caja/scripts
+	fi
+	mkdir -p ~/.config/caja/scripts
+	cp ./diff_with ~/.config/caja/scripts
 }
 
 cd ${BASEDIR}
