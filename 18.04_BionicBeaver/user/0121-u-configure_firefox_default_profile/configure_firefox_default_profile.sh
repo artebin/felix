d227589cba0b12c59d07e54780bc427a7cb905c0
Nicolas James <nicolas.james@gmail.com>
2018-06-09 15:48:17 +0200

    improved default firefox configuration

--- 18.04_BionicBeaver/user/0121-u-configure_firefox_default_profile/configure_firefox_default_profile.sh
+++ 18.04_BionicBeaver/user/0121-u-configure_firefox_default_profile/configure_firefox_default_profile.sh
@@ -34,8 +34,8 @@ configure_firefox_default_profile(){
 	
 	echo "Setting \"Show your windows and tabs from last time\" ..."
 	KEY="browser.startup.page"
-	PREFIX_TO_SEARCH="user_pref(\"browser.startup.page\""
-	LINE_REPLACEMENT_VALUE="user_pref(\"browser.startup.page\", 3);"
+	PREFIX_TO_SEARCH="user_pref(\"${KEY}\""
+	LINE_REPLACEMENT_VALUE="user_pref(\"${KEY}\", 3);"
 	if grep -q "${KEY}" "${PREFS_JS_PATH}"; then
 		sed -i "/^${PREFIX_TO_SEARCH}/s/.*/${LINE_REPLACEMENT_VALUE}/" "${PREFS_JS_PATH}"
 	else
@@ -44,8 +44,8 @@ configure_firefox_default_profile(){
 	
 	echo "Setting \"Always ask you where to save files\" ..."
 	KEY="browser.download.useDownloadDir"
-	PREFIX_TO_SEARCH="user_pref(\"browser.download.useDownloadDir\""
-	LINE_REPLACEMENT_VALUE="user_pref(\"browser.download.useDownloadDir\", false);"
+	PREFIX_TO_SEARCH="user_pref(\"${KEY}\""
+	LINE_REPLACEMENT_VALUE="user_pref(\"${KEY}\", false);"
 	if grep -q "${KEY}" "${PREFS_JS_PATH}"; then
 		sed -i "/^${PREFIX_TO_SEARCH}/s/.*/${LINE_REPLACEMENT_VALUE}/" "${PREFS_JS_PATH}"
 	else
@@ -54,8 +54,8 @@ configure_firefox_default_profile(){
 	
 	echo "Setting \"Do not remember logins and passwords\" ..."
 	KEY="signon.rememberSignons"
-	PREFIX_TO_SEARCH="user_pref(\"signon.rememberSignons\""
-	LINE_REPLACEMENT_VALUE="user_pref(\"signon.rememberSignons\", false);"
+	PREFIX_TO_SEARCH="user_pref(\"${KEY}\""
+	LINE_REPLACEMENT_VALUE="user_pref(\"${KEY}\", false);"
 	if grep -q "${KEY}" "${PREFS_JS_PATH}"; then
 		sed -i "/^${PREFIX_TO_SEARCH}/s/.*/${LINE_REPLACEMENT_VALUE}/" "${PREFS_JS_PATH}"
 	else
@@ -64,8 +64,8 @@ configure_firefox_default_profile(){
 	
 	echo "Setting \"Do not trim URLs in the URL bar\" ..."
 	KEY="browser.urlbar.trimURLs"
-	PREFIX_TO_SEARCH="user_pref(\"browser.urlbar.trimURLs\""
-	LINE_REPLACEMENT_VALUE="user_pref(\"browser.urlbar.trimURLs\", false);"
+	PREFIX_TO_SEARCH="user_pref(\"${KEY}\""
+	LINE_REPLACEMENT_VALUE="user_pref(\"${KEY}\", false);"
 	if grep -q "${KEY}" "${PREFS_JS_PATH}"; then
 		sed -i "/^${PREFIX_TO_SEARCH}/s/.*/${LINE_REPLACEMENT_VALUE}/" "${PREFS_JS_PATH}"
 	else
@@ -74,15 +74,43 @@ configure_firefox_default_profile(){
 	
 	echo "Setting \"Do not show search engines in drop panel of the URL bar\" ..."
 	KEY="browser.urlbar.oneOffSearches"
-	PREFIX_TO_SEARCH="user_pref(\"browser.urlbar.oneOffSearches\""
-	LINE_REPLACEMENT_VALUE="user_pref(\"browser.urlbar.oneOffSearches\", false);"
+	PREFIX_TO_SEARCH="user_pref(\"${KEY}\""
+	LINE_REPLACEMENT_VALUE="user_pref(\"${KEY}\", false);"
+	if grep -q "${KEY}" "${PREFS_JS_PATH}"; then
+		sed -i "/^${PREFIX_TO_SEARCH}/s/.*/${LINE_REPLACEMENT_VALUE}/" "${PREFS_JS_PATH}"
+	else
+		echo "${LINE_REPLACEMENT_VALUE}" >> "${PREFS_JS_PATH}"
+	fi
+	
+	echo "Setting \"No search suggestions in the URL bar\" ..."
+	# Part 1
+	KEY="browser.urlbar.searchSuggestionsChoice"
+	PREFIX_TO_SEARCH="user_pref(\"${KEY}\""
+	LINE_REPLACEMENT_VALUE="user_pref(\"${KEY}\", false);"
+	if grep -q "${KEY}" "${PREFS_JS_PATH}"; then
+		sed -i "/^${PREFIX_TO_SEARCH}/s/.*/${LINE_REPLACEMENT_VALUE}/" "${PREFS_JS_PATH}"
+	else
+		echo "${LINE_REPLACEMENT_VALUE}" >> "${PREFS_JS_PATH}"
+	fi
+	# Part 2
+	KEY="browser.urlbar.suggest.searches"
+	PREFIX_TO_SEARCH="user_pref(\"${KEY}\""
+	LINE_REPLACEMENT_VALUE="user_pref(\"${KEY}\", false);"
+	if grep -q "${KEY}" "${PREFS_JS_PATH}"; then
+		sed -i "/^${PREFIX_TO_SEARCH}/s/.*/${LINE_REPLACEMENT_VALUE}/" "${PREFS_JS_PATH}"
+	else
+		echo "${LINE_REPLACEMENT_VALUE}" >> "${PREFS_JS_PATH}"
+	fi
+	# Part 3
+	KEY="browser.urlbar.timesBeforeHidingSuggestionsHint"
+	PREFIX_TO_SEARCH="user_pref(\"${KEY}\""
+	LINE_REPLACEMENT_VALUE="user_pref(\"${KEY}\", 0);"
 	if grep -q "${KEY}" "${PREFS_JS_PATH}"; then
 		sed -i "/^${PREFIX_TO_SEARCH}/s/.*/${LINE_REPLACEMENT_VALUE}/" "${PREFS_JS_PATH}"
 	else
 		echo "${LINE_REPLACEMENT_VALUE}" >> "${PREFS_JS_PATH}"
 	fi
 }
-
 cd ${BASEDIR}
 configure_firefox_default_profile 2>&1 | tee -a ./${CURRENT_SCRIPT_LOG_FILE_NAME}
 EXIT_CODE="${PIPESTATUS[0]}"
