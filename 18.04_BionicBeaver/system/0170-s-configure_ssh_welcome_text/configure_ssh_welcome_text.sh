fb293235e445ffb3c3f658fd36a3740c048f592a
Nicolas James <nicolas.james@gmail.com>
2018-09-06 11:17:52 +0200

    system recipe 0170 should disable existing motd

--- 18.04_BionicBeaver/system/0170-s-configure_ssh_welcome_text/configure_ssh_welcome_text.sh
+++ 18.04_BionicBeaver/system/0170-s-configure_ssh_welcome_text/configure_ssh_welcome_text.sh
@@ -8,6 +8,15 @@ configure_ssh_welcome_text(){
 	cd ${BASEDIR}
 	
 	echo "Setting SSH welcome text ..."
+	
+	# Disabling all previous "message of the day"
+	cd /etc/update-motd.d
+	for FILE in ./*; do
+		backup_file rename ./"${FILE}"
+	done
+	
+	# Adding Tux Welcome Dude
+	cd ${BASEDIR}
 	cp ./00-welcome-dude /etc/update-motd.d/00-welcome-dude
 	cp ./tux /etc/update-motd.d/tux
 	cd /etc/update-motd.d
