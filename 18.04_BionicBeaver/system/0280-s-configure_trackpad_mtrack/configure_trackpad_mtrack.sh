44e2b68bbfc111ae78db9b79cc4690ceb241f4f0
Nicolas James <nicolas.james@gmail.com>
2018-10-25 22:15:25 +0200

    recipe "configure trackpad mtrack" should be executed

--- 18.04_BionicBeaver/system/0280-s-configure_trackpad_mtrack/configure_trackpad_mtrack.sh
+++ 18.04_BionicBeaver/system/0280-s-configure_trackpad_mtrack/configure_trackpad_mtrack.sh
@@ -9,6 +9,18 @@ configure_trackpad_mtrack(){
 	echo "Configuring trackpad mtrack ..."
 	
 	apt-get install -y xserver-xorg-input-mtrack
+	
+	# For reference, the X11 mouse button numbering:
+	#   1 = left button
+	#   2 = middle button (pressing the scroll wheel)
+	#   3 = right button
+	#   4 = turn scroll wheel up
+	#   5 = turn scroll wheel down
+	#   6 = push scroll wheel left
+	#   7 = push scroll wheel right
+	#   8 = 4th button (aka browser backward button)
+	#   9 = 5th button (aka browser forward button)
+	
 	cp ./80-mtrack.conf /usr/share/X11/xorg.conf.d/
 }
 
