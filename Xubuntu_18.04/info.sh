e2be4bdae1f924b285f3404c3b7b15e9369a52d8
Nicolas James <nicolas.james@gmail.com>
2018-12-16 12:34:24 +0100

    improved info.sh

--- Xubuntu_18.04/info.sh
+++ Xubuntu_18.04/info.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 source ./common.sh
+check_shell
+exit_if_has_not_root_privileges
 
 ################
 # Product name
@@ -9,47 +11,59 @@ print_section_heading "Product name"
 sudo dmidecode -s system-product-name
 print_section_ending
 
+###################
+# RAM information
+###################
+print_section_heading "RAM information (demidecode --type memory)"
+sudo dmidecode --type memory
+print_section_ending
+
+############
+# Displays
+############
+print_section_heading "Displays"
+xrandr --current
+print_section_ending
+
 #####################
 # Distribution name
 #####################
+print_section_heading "Distribution name"
 lsb_release -a
+print_section_ending
 
 ##################
 # Ubuntu version
 ##################
-print_section_heading "Ubuntu Linux"
-INSTALLER_MEDIA_INFO_PATH='/var/log/installer/media-info'
-if [[ -f "${INSTALLER_MEDIA_INFO_PATH}" ]]; then
-	printf "Ubuntu Linux detected:\n$(cat /var/log/installer/media-info)\n"
-else
-	echo "This is not Ubuntu"
-fi
+print_section_heading "Ubuntu Version (cat /var/log/installer/media-info)"
+cat /var/log/installer/media-info
 print_section_ending
 
 ########################
 # Linux kernel version
 ########################
+print_section_heading "Linux Kernel Version"
+print_section_ending
+
+###############
+# Memory info
+###############
+print_section_heading "Memory info (cat /proc/meminfo)"
+cat /proc/meminfo
+print_section_ending
+
+#########################
+# Free memory (free -m)
+#########################
+print_section_heading "Free memory (free -m)"
+free -m
+print_section_ending
 
 #######################
 # Temperature sensors
 #######################
 print_section_heading "Temperature sensors"
 sensors
-print_section_ending
-
-###################
-# RAM information
-###################
-print_section_heading "/proc/meminfo"
-cat /proc/meminfo
-print_section_ending
-
-print_section_heading "demidecode --type memory"
-sudo dmidecode --type memory
-print_section_ending
-
-print_section_heading "Free memory"
-free -m
 print_section_ending
 
 #####################
@@ -65,7 +79,3 @@ print_section_ending
 print_section_heading "GTK+ version"
 dpkg -l libgtk2.0-0 libgtk-3-0
 print_section_ending
-
-# Java version
-
-# Screens number and resolution
