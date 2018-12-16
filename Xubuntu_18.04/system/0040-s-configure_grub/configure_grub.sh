d7eac8270f08f8c63187f9823af3fc07a2da8307
Nicolas James <nicolas.james@gmail.com>
2018-12-16 15:50:24 +0100

    recipe 0040-s configure grub adds the resume=UUID=<first swap partition UUID>

--- Xubuntu_18.04/system/0040-s-configure_grub/configure_grub.sh
+++ Xubuntu_18.04/system/0040-s-configure_grub/configure_grub.sh
@@ -7,18 +7,27 @@ exit_if_has_not_root_privileges
 configure_grub(){
 	cd ${BASEDIR}
 	
-	echo "Configuring grub ..."
-	
 	# Backup grub configuration
+	echo "Backup current grub configuration ..."
 	backup_file copy /etc/default/grub
+	echo
 	
 	# Remove hidden timeout 0 => show grub
-	echo "Remove hidden timeout 0 => show grub"
+	echo "Remove timeout style => show grub ..."
 	sed -i "/^GRUB_TIMEOUT_STYLE/s/.*/#GRUB_TIMEOUT_STYLE=/" /etc/default/grub
+	echo
 	
-	# Remove boot option "quiet" and "splash"
+	# Remove boot option 'quiet' and 'splash'
 	echo "Remove boot option 'quiet' and 'splash' ..."
 	sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/s/.*/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/" /etc/default/grub
+	echo
+	
+	# Add swap partition for resume from hibernate
+	echo "Add swap partition for resume from hibernate ..."
+	FIRST_SWAP_PARTITION_DEVICE_NAME=$(swapon --noheadings --raw --show=NAME|head -n1)
+	FIRST_SWAP_PARTITION_UUID=$(sudo blkid -s UUID -o value ${FIRST_SWAP_PARTITION_DEVICE_NAME})
+	sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/s/.*/GRUB_CMDLINE_LINUX_DEFAULT=\"resume=UUID=${FIRST_SWAP_PARTITION_UUID}\"/" /etc/default/grub
+	echo
 	
 	update-grub
 	
