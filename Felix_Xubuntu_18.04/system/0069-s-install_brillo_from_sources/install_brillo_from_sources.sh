cde739010877d4e3b406c1febba7704ff3329e85
Nicolas James <nicolas.james@gmail.com>
2018-12-26 17:31:01 +0100

    added recipe 0069-s install_brillo_from_sources

---
+++ Felix_Xubuntu_18.04/system/0069-s-install_brillo_from_sources/install_brillo_from_sources.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source ../../Felix_Xubuntu_18.04.sh
+is_bash
+exit_if_has_not_root_privileges
+
+install_brillo_from_sources(){
+	echo "Install brillo from sources ..."
+	
+	cd ${BASEDIR}
+	git clone https://gitlab.com/cameronnemo/brillo
+	cd brillo
+	make
+	make install
+	make dist
+	make install-dist
+	
+	# Cleaning
+	cd ${BASEDIR}
+	rm -fr brillo
+	
+	echo
+}
+
+cd ${BASEDIR}
+
+install_brillo_from_sources 2>&1 | tee -a ./${CURRENT_SCRIPT_LOG_FILE_NAME}
+EXIT_CODE="${PIPESTATUS[0]}"
+if [ "${EXIT_CODE}" -ne 0 ]; then
+	exit "${EXIT_CODE}"
+fi
