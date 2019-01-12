a275d1f1edba99bae0dd75fd88e0161fec9de68f
Nicolas James <nicolas.james@gmail.com>
2019-01-12 13:20:26 +0100

    added recipe 'install_pulseaudioctl_from_sources'

---
+++ xubuntu_18.04/system/0037-s-install_pulseaudioctl_from_sources/install_pulseaudioctl_from_sources.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source ../../xubuntu_18.04.sh
+is_bash
+exit_if_has_not_root_privileges
+
+install_pulseaudioctl_from_sources(){
+	echo "Installing pulseaudio-ctl from sources ..."
+	
+	# Clone git repository
+	git clone https://github.com/graysky2/pulseaudio-ctl
+	
+	# Build and install
+	cd pulseaudio-ctl
+	make
+	make install
+	
+	# Cleaning
+	cd ${BASEDIR}
+	rm -fr pulseaudio-ctl
+	
+	echo
+}
+
+cd ${BASEDIR}
+install_pulseaudioctl_from_sources 2>&1 | tee -a ./${CURRENT_SCRIPT_LOG_FILE_NAME}
+EXIT_CODE="${PIPESTATUS[0]}"
+if [ "${EXIT_CODE}" -ne 0 ]; then
+	exit "${EXIT_CODE}"
+fi
