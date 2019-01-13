eec5daa93f7ee4ba312c3fbc1e485f4e9b0f2f61
Nicolas James <nicolas.james@gmail.com>
2019-01-13 17:19:24 +0100

    minor refactoring

--- ubuntu_18.04/system/0160-s-install_fixed_notify_send/install_fixed_notify_send.sh
+++ ubuntu_18.04/system/0160-s-install_fixed_notify_send/install_fixed_notify_send.sh
@@ -4,15 +4,19 @@ source ../../ubuntu_18.04.sh
 is_bash
 exit_if_has_not_root_privileges
 
-fix_notify_send(){
-	echo "Fix notify-send ..."
+install_fixed_notify_send(){
+	echo "Install a fixed version of notify-send ..."
 	
 	if $(is_package_installed "libnotify-bin"); then
 		apt-get remove -y "libnotify-bin"
 	fi
 	
+	# Clone git repository
 	cd ${BASEDIR}
 	git clone https://github.com/vlevit/notify-send.sh
+	
+	# Install
+	cd ${BASEDIR}
 	cd notify-send.sh
 	cp notify-send.sh /usr/bin/notify-send
 	cp notify-action.sh /usr/bin/notify-action
@@ -25,8 +29,7 @@ fix_notify_send(){
 }
 
 cd ${BASEDIR}
-
-fix_notify_send 2>&1 | tee -a ./${CURRENT_SCRIPT_LOG_FILE_NAME}
+install_fixed_notify_send 2>&1 | tee -a ./${CURRENT_SCRIPT_LOG_FILE_NAME}
 EXIT_CODE="${PIPESTATUS[0]}"
 if [ "${EXIT_CODE}" -ne 0 ]; then
 	exit "${EXIT_CODE}"
