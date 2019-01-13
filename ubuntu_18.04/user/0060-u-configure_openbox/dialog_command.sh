2a775ff4673b2c3512241e8aff7a4f8dc7bf328f
Nicolas James <nicolas.james@gmail.com>
2019-01-13 17:42:08 +0100

    minor changes

--- ubuntu_18.04/user/0060-u-configure_openbox/dialog_command.sh
+++ ubuntu_18.04/user/0060-u-configure_openbox/dialog_command.sh
@@ -1,6 +1,6 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 DIALOG_RETURN_CODE=$(zenity --no-wrap --question --text "${1}"; echo $?)
 if [ ${DIALOG_RETURN_CODE} = 0 ]; then
-  exec ${2}
+	exec ${2}
 fi
--- ubuntu_18.04/user/0060-u-configure_openbox/dialog_command2.sh
+++ ubuntu_18.04/user/0060-u-configure_openbox/dialog_command2.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 # This will wait one second and then steal focus and make the Zenity dialog box always-on-top (aka. 'above').
 (sleep 1 && wmctrl -F -a "${1}" -b add,above) &
