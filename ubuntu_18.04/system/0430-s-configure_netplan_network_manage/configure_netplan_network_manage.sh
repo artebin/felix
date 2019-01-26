9d13fd6b3a82fab7068a9521391598634faae4da
Nicolas James <nicolas.james@gmail.com>
2019-01-26 12:38:12 +0100

    added recipe 0430-s force netplan to use the NetworkManager

---
+++ ubuntu_18.04/system/0430-s-configure_netplan_network_manage/configure_netplan_network_manage.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source ../../ubuntu_18.04.sh
+is_bash
+exit_if_has_not_root_privileges
+
+configure_netplan_network_manage(){
+	echo "Force netplan to use the NetworkManager ..."
+	
+	add_or_update_line_based_on_prefix "  renderer: networkd" "  renderer: NetworkManager" /etc/netplan/01-netcfg.yaml
+	netplan generate
+	netplan apply
+	
+	echo
+}
+
+cd ${BASEDIR}
+configure_netplan_network_manage 2>&1 | tee -a ./${CURRENT_SCRIPT_LOG_FILE_NAME}
+EXIT_CODE="${PIPESTATUS[0]}"
+if [ "${EXIT_CODE}" -ne 0 ]; then
+	exit "${EXIT_CODE}"
+fi
