21239c652ac805ff929f0951d8be2888cff74643
Nicolas James <nicolas.james@gmail.com>
2019-02-15 18:38:09 +0100

    enh: added rofi configuration

---
+++ ubuntu_1804/user/0101-u-configure_rofi/configure_rofi.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+source ../../../felix.sh
+source ../../ubuntu_1804.conf
+
+BASEDIR="$(dirname ${BASH_SOURCE})"
+LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE})"
+
+is_bash
+
+configure_rofi(){
+	echo "Configure rofi ..."
+	
+	cd "${BASEDIR}"
+	if [[ -f ~/.config/rofi ]]; then
+		backup_file rename ~/.config/rofi
+	fi
+	if [[ ! -f ~/.config/rofi ]]; then
+		mkdir -p ~/.config/rofi
+	fi
+	cp ./rofi_1.5.0_config ~/.config/rofi/config
+	
+	echo
+}
+
+
+cd "${BASEDIR}"
+configure_rofi 2>&1 | tee -a "${LOGFILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
