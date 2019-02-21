92970bda5295135a4b4951459692d18522b9f593
Nicolas James <nicolas.james@gmail.com>
2019-02-21 09:15:27 +0100

    fixed user script for starting fdpowermon

--- ubuntu_1804/user/0220-u-install_user_scripts/scripts/fdpowermon_run.sh
+++ ubuntu_1804/user/0220-u-install_user_scripts/scripts/fdpowermon_run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-ACPI_OUTPUT="$(acpi -V 2>&1)"
-NO_POWER_SUPPLY_REGEX="^No support for device type: power_supply$"
-if [[ ! "${ACPI_OUTPUT}" =~ ${NO_POWER_SUPPLY_REGEX} ]]; then
+BATTERY_INFO=$(upower -e|grep 'BAT')
+if [[ ! -z "${BATTERY_INFO}" ]]; then
 	fdpowermon &
 fi
