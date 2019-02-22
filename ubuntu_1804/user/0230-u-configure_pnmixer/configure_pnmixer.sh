4dc85ba2025d6d122e93d0141424159dca3ae42b
Nicolas James <nicolas.james@gmail.com>
2019-02-22 17:30:32 +0100

    install pnmixer by default + added recipe "configure_pnmixer"

---
+++ ubuntu_1804/user/0230-u-configure_pnmixer/configure_pnmixer.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+RECIPE_DIR="$(dirname ${BASH_SOURCE}|xargs readlink -f)"
+FELIX_ROOT="${RECIPE_DIR%/felix/*}/felix"
+if [[ ! -f "${FELIX_ROOT}/felix.sh" ]]; then
+	printf "Cannot find ${FELIX_ROOT}/felix.sh\n"
+	exit 1
+fi
+source "${FELIX_ROOT}/felix.sh"
+RECIPE_FAMILY_DIR=$(retrieve_recipe_family_dir "${RECIPE_DIR}")
+RECIPE_FAMILY_CONF_FILE=$(retrieve_recipe_family_conf_file "${RECIPE_DIR}")
+if [[ ! -f "${RECIPE_FAMILY_CONF_FILE}" ]]; then
+	printf "Cannot find RECIPE_FAMILY_CONF_FILE: ${RECIPE_FAMILY_CONF_FILE}\n"
+	exit 1
+fi
+source "${RECIPE_FAMILY_CONF_FILE}"
+LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE}|xargs readlink -f)"
+
+exit_if_not_bash
+
+configure_pnmixer(){
+	echo "Configuring pnmixer ..."
+	
+	if [[ -d "${HOME}/.config/pnmixer" ]]; then
+		backup_file rename "${HOME}/.config/pnmixer"
+	fi
+	
+	mkdir -p "${HOME}/.config/pnmixer"
+	cp "${RECIPE_FAMILY_DIR}/dotfiles/.config/pnmixer/config" "${HOME}/.config/pnmixer"
+	
+	echo
+}
+
+configure_pnmixer 2>&1 | tee -a "${LOGFILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
