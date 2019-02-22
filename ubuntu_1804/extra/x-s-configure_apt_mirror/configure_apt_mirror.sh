5a02d29e51a3fdd711fc99fa5795a332d07d89b3
Nicolas James <nicolas.james@gmail.com>
2019-02-22 10:32:44 +0100

    enh recipe "configure_apt_mirror"

--- ubuntu_1804/extra/x-s-configure_apt_mirror/configure_apt_mirror.sh
+++ ubuntu_1804/extra/x-s-configure_apt_mirror/configure_apt_mirror.sh
@@ -19,26 +19,45 @@ LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE}|xargs readlink -f)"
 exit_if_not_bash
 exit_if_has_not_root_privileges
 
+APT_MIRROR_BASE_PATH="/media/njames/FONDA-EXT4/UbuntuMirror/Bionic"
+LOCAL_MIRROR_ADDRESS="localhost:10001"
+
 configure_apt_mirror(){
 	echo "Configuring apt-mirror ..."
-	cd "${RECIPE_DIR}"
 	if [[ -f /etc/apt/mirror.list ]]; then
 		backup_file rename /etc/apt/mirror.list
 	fi
+	cd "${RECIPE_DIR}"
+	sed -i "s/APT_MIRROR_BASE_PATH/${APT_MIRROR_BASE_PATH}/g" apt.mirror.list
 	cp apt.mirror.list /etc/apt/mirror.list
 	
-	echo "Adding the local mirror into the sources ..."
-	cd "${RECIPE_DIR}"
+	printf "Once the mirror is complete, start a http server for the repository:\n"
+	printf "  - go into APT_MIRROR_BASE_PATH/mirror/archive.ubuntu.com\n"
+	printf "  - execute 'python -m SimpleHTTPServer 10001'\n"
+	
+	echo
+}
+
+add_local_mirror_in_apt_sources(){
+	echo "Adding the local mirror in APT sources ..."
 	if [[ -f /etc/apt/sources.list.d/local_mirror.list ]]; then
 		backup_file rename /etc/apt/sources.list.d/local_mirror.list
 	fi
-	cp apt.mirror.list /etc/apt/sources.list.d/local_mirror.list
-	
+	cd "${RECIPE_DIR}"
+	sed -i "s/LOCAL_MIRROR_ADDRESS/${LOCAL_MIRROR_ADDRESS}/g" apt.sources.list
+	cp apt.sources.list /etc/apt/sources.list.d/local_mirror.list
 	echo
 }
 
 cd "${RECIPE_DIR}"
 configure_apt_mirror 2>&1 | tee -a "${LOGFILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
+
+cd "${RECIPE_DIR}"
+add_local_mirror_in_apt_sources 2>&1 | tee -a "${LOGFILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
