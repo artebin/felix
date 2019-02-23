975e12d30d41fc882c952055fe5ceac63cc8b195
Nicolas James <nicolas.james@gmail.com>
2019-02-23 15:19:25 +0100

    moved openbox conf files in ubuntu_1804/dotfiles + moved tile.sh with files in user scripts dir

--- ubuntu_1804/user/0060-u-configure_openbox/configure_openbox.sh
+++ ubuntu_1804/user/0060-u-configure_openbox/configure_openbox.sh
@@ -20,22 +20,16 @@ exit_if_not_bash
 
 configure_openbox(){
 	echo "Configuring openbox ..."
-	
-	if [[ -d ~/.config/openbox ]]; then
-		backup_file rename ~/.config/openbox
+	if [[ -d "${HOME}/.config/openbox" ]]; then
+		backup_file rename "${HOME}/.config/openbox"
 	fi
-	mkdir -p ~/.config/openbox
-	
-	cd "${RECIPE_DIR}"
-	cp autostart ~/.config/openbox
-	cp rc.xml ~/.config/openbox
-	cp menu.xml ~/.config/openbox
-	cp obapps-0.1.7.tar.gz ~/.config/openbox
-	
+	mkdir -p "${HOME}/.config/openbox"
+	cp "${RECIPE_FAMILY_DIR}/dotfiles/.config/openbox/autostart" "${HOME}/.config/openbox"
+	cp "${RECIPE_FAMILY_DIR}/dotfiles/.config/openbox/rc.xml" "${HOME}/.config/openbox"
+	cp "${RECIPE_FAMILY_DIR}/dotfiles/.config/openbox/menu.xml" "${HOME}/.config/openbox"
 	echo
 }
 
-cd "${RECIPE_DIR}"
 configure_openbox 2>&1 | tee -a "${LOGFILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
