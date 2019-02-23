f891604727a1d3b45a1d91f8a444ec3923f6516c
Nicolas James <nicolas.james@gmail.com>
2019-02-23 16:44:14 +0100

    cleaned recipe "configure_mate_caja"

--- ubuntu_1804/user/0150-u-configure_mate_caja/configure_mate_caja.sh
+++ ubuntu_1804/user/0150-u-configure_mate_caja/configure_mate_caja.sh
@@ -19,38 +19,32 @@ LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE}|xargs readlink -f)"
 exit_if_not_bash
 
 configure_mate_caja(){
-	cd ${RECIPE_DIR}
-	
 	echo "Configuring mate-caja ..."
+	dconf load /org/mate/caja/ <"${RECIPE_DIR}/org.mate.caja.dump"
+	dconf load /org/mate/desktop/ <"${RECIPE_DIR}/org.mate.desktop.dump"
+	echo
 	
-	echo "Importing caja configuration in dconf ..."
-	dconf load /org/mate/caja/ < ./org.mate.caja.dump
-	dconf load /org/mate/desktop/ < ./org.mate.desktop.dump
-	
+	echo "Use x-terminal-emulator for the action 'Open in Terminal' ..."
 	# For an unkown reason caja-open-terminal plugin is not using
 	# x-terminal-emulator by default.
 	# See <https://github.com/mate-desktop/caja-extensions/blob/master/open-terminal/caja-open-terminal.c>
-	echo "Use x-terminal-emulator for \"Open in Terminal\" action ..."
 	gsettings set org.mate.applications-terminal exec x-terminal-emulator
+	echo
 	
 	echo "Adding caja scripts ..."
-	if [ -d ~/.config/caja/scripts ]; then
-	backup_file rename ~/.config/caja/scripts
+	if [[ -d "${HOME}/.config/caja/scripts" ]]; then
+		backup_file rename "${HOME}/.config/caja/scripts"
 	fi
-	mkdir -p ~/.config/caja/scripts
-	cp ./compare ~/.config/caja/scripts
-	cp ./tail_and_follow ~/.config/caja/scripts
-	cp ./tail_entirely_and_follow ~/.config/caja/scripts
-	cp -R ./indentation ~/.config/caja/scripts
-	
+	mkdir -p "${HOME}/.config/caja/scripts"
+	cp "${RECIPE_FAMILY_DIR}/dotfiles/.config/caja/scripts/compare" "${HOME}/.config/caja/scripts"
+	cp "${RECIPE_FAMILY_DIR}/dotfiles/.config/caja/scripts/tail_and_follow" "${HOME}/.config/caja/scripts"
+	cp "${RECIPE_FAMILY_DIR}/dotfiles/.config/caja/scripts/tail_entirely_and_follow" "${HOME}/.config/caja/scripts"
+	cp -R "${RECIPE_FAMILY_DIR}/dotfiles/.config/caja/scripts/indentation" "${HOME}/.config/caja/scripts"
 	echo
 }
 
-
-
-cd ${RECIPE_DIR}
 configure_mate_caja 2>&1 | tee -a "${LOGFILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
-if [ "${EXIT_CODE}" -ne 0 ]; then
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
 fi
