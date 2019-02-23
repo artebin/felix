f77fb77074c2fc5071bd9522c3ea187d3ff5e18e
Nicolas James <nicolas.james@gmail.com>
2019-02-23 16:43:44 +0100

    cleaned recipe "configure_mate_terminal"

--- ubuntu_1804/user/0140-u-configure_mate_terminal/configure_mate_terminal.sh
+++ ubuntu_1804/user/0140-u-configure_mate_terminal/configure_mate_terminal.sh
@@ -19,19 +19,13 @@ LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE}|xargs readlink -f)"
 exit_if_not_bash
 
 configure_mate_terminal(){
-	cd ${RECIPE_DIR}
-	
 	echo "Configuring mate-terminal ..."
-	dconf load /org/mate/terminal/ < ./org.mate.terminal.dump
-	
+	dconf load /org/mate/terminal/ <"${RECIPE_DIR}/org.mate.terminal.dump"
 	echo
 }
 
-
-
-cd ${RECIPE_DIR}
 configure_mate_terminal 2>&1 | tee -a "${LOGFILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
-if [ "${EXIT_CODE}" -ne 0 ]; then
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
 fi
