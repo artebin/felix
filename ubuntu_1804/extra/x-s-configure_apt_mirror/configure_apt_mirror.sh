41edae264d09016487b1be929ce47bd0c7e6852c
Nicolas James <nicolas.james@gmail.com>
2019-02-27 18:16:09 +0100

    splitted recipe "configure_apt_mirror" with "add_local_mirror_in_apt_sources"

--- ubuntu_1804/extra/x-s-configure_apt_mirror/configure_apt_mirror.sh
+++ ubuntu_1804/extra/x-s-configure_apt_mirror/configure_apt_mirror.sh
@@ -20,7 +20,6 @@ exit_if_not_bash
 exit_if_has_not_root_privileges
 
 APT_MIRROR_BASE_PATH="/media/njames/FONDA-EXT4/UbuntuMirror/Bionic"
-LOCAL_MIRROR_ADDRESS="localhost:10001"
 
 configure_apt_mirror(){
 	echo "Configuring apt-mirror ..."
@@ -37,38 +36,8 @@ configure_apt_mirror(){
 	echo
 }
 
-add_local_mirror_in_apt_sources(){
-	echo "Adding the local mirror in APT sources ..."
-	if [[ -f /etc/apt/sources.list.d/local_mirror.list ]]; then
-		backup_file rename /etc/apt/sources.list.d/local_mirror.list
-	fi
-	cp "${RECIPE_DIR}/apt.sources.list" /etc/apt/sources.list.d/local_mirror.list
-	sed -i "s|LOCAL_MIRROR_ADDRESS|${LOCAL_MIRROR_ADDRESS}|g" /etc/apt/sources.list.d/local_mirror.list
-	echo
-}
-
-comment_all_default_apt_sources(){
-	printf "Commenting all default sources in /etc/apt/sources.list ...\n"
-	sed -i 's|^\([^#]\)|#\1|g' /etc/apt/sources.list
-	printf "\n"
-}
-
 cd "${RECIPE_DIR}"
 configure_apt_mirror 2>&1 | tee -a "${LOGFILE}"
-EXIT_CODE="${PIPESTATUS[0]}"
-if [[ "${EXIT_CODE}" -ne 0 ]]; then
-	exit "${EXIT_CODE}"
-fi
-
-cd "${RECIPE_DIR}"
-add_local_mirror_in_apt_sources 2>&1 | tee -a "${LOGFILE}"
-EXIT_CODE="${PIPESTATUS[0]}"
-if [[ "${EXIT_CODE}" -ne 0 ]]; then
-	exit "${EXIT_CODE}"
-fi
-
-cd "${RECIPE_DIR}"
-comment_all_default_apt_sources 2>&1 | tee -a "${LOGFILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
