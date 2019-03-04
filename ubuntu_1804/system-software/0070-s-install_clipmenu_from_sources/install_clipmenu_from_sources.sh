8e10d2b4228685c3ebfd2453fa8fbeefb5f26c91
Nicolas James <nicolas.james@gmail.com>
2019-03-04 20:20:47 +0100

    added patch on clipmenud for copy/paste in file managers + fixed clipmenud_run.sh

--- ubuntu_1804/system-software/0070-s-install_clipmenu_from_sources/install_clipmenu_from_sources.sh
+++ ubuntu_1804/system-software/0070-s-install_clipmenu_from_sources/install_clipmenu_from_sources.sh
@@ -19,49 +19,45 @@ LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE}|xargs readlink -f)"
 exit_if_not_bash
 exit_if_has_not_root_privileges
 
-install_clipmenu_from_sources(){
-	cd ${RECIPE_DIR}
-	
-	# ClipNotify
-	cd ${RECIPE_DIR}
+install_clipnotify_from_sources(){
 	echo "Installing ClipNotify from sources ..."
 	echo "GitHub repository: <https://github.com/cdown/clipnotify>"
 	git clone https://github.com/cdown/clipnotify
-	cd ./clipnotify
+	cd "${RECIPE_DIR}/clipnotify"
 	make
-	cp ./clipnotify /usr/bin
-	
-	# ClipMenu
-	cd ${RECIPE_DIR}
-	echo "Installing ClipMenu from sources ..."
-	echo "GitHub repository: <https://github.com/cdown/clipmenu>"
-	git clone https://github.com/cdown/clipmenu
-	cd ./clipmenu
-	cp ./clipdel /usr/bin
-	cp ./clipmenu /usr/bin
-	cp ./clipmenud /usr/bin
-	
-	# I can not make it work with the service
-	# It will be executed via openbox autostart
-	#cp ./init/clipmenud.service /etc/systemd/system
-	#systemctl daemon-reload
-	#systemctl start clipmenud.service
-	#systemctl status clipmenud.service
-	#systemctl enable clipmenud.service
+	cp clipnotify /usr/bin
 	
 	# Cleaning
-	cd ${RECIPE_DIR}
-	rm -fr ./clipnotify
-	rm -fr ./clipmenu
+	rm -fr "${RECIPE_DIR}/clipnotify"
 	
 	echo
 }
 
+install_clipmenu_from_sources(){
+	echo "Installing ClipMenu from sources ..."
+	echo "GitHub repository: <https://github.com/cdown/clipmenu>"
+	git clone https://github.com/cdown/clipmenu
+	cd "${RECIPE_DIR}/clipmenu"
+	printf "Applying patch on clipmenud for copy/paste in file managers ...\n"
+	patch < ../clipmenud_190204.patch
+	cp clipdel /usr/bin
+	cp clipmenu /usr/bin
+	cp clipmenud /usr/bin
+	
+	# Cleaning
+	rm -fr "${RECIPE_DIR}/clipmenu"
+	
+	echo
+}
 
+install_clipnotify_from_sources 2>&1 | tee -a "${LOGFILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
 
-cd ${RECIPE_DIR}
 install_clipmenu_from_sources 2>&1 | tee -a "${LOGFILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
-if [ "${EXIT_CODE}" -ne 0 ]; then
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
 fi
--- ubuntu_1804/user/0220-u-install_user_scripts/scripts/clipmenud_run.sh
+++ ubuntu_1804/user/0220-u-install_user_scripts/scripts/clipmenud_run.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-# Kill all running instances of clipmenud or clipnotify
-CLIPMENUD_PIDS=$(ps -C clipmenud -o pid=)
-if [[ ! -z "${CLIPMENUD_PIDS}" ]]; then
-	kill -9 "${CLIPMENUD_PIDS}"
-fi
-CLIPNOTIFY_PIDS=$(ps -C clipnotify -o pid=)
-if [[ ! -z "${CLIPNOTIFY_PIDS}" ]]; then
-	kill -9 "${CLIPNOTIFY_PIDS}"
-fi
+# Kill all running instances of clipmenud
+kill $(ps -u|grep 'bash /usr/bin/clipmenud'|awk '{print $2}') 2>/dev/null
+kill $(ps -u|grep 'clipnotify'|awk '{print $2}') 2>/dev/null
 
-# Take ownership of the clipboard
+#~ # Take ownership of the clipboard
 export CM_OWN_CLIPBOARD=1
 
-# Manage the clipboard only
+#~ # Manage the clipboard only
 export CM_SELECTIONS="clipboard"
 
 export CM_DEBUG=1
 
-clipmenud >"${HOME}/clipmenud.log" 2>&1 &
+nohup clipmenud >"${HOME}/clipmenud.log" 2>&1 &
