e84c93d4d5a2ab7eace1ffaeaf94baa709b9e836
Nicolas James <nicolas.james@gmail.com>
2019-03-11 13:43:59 +0100

    improved 'configure_default_applications'

--- ubuntu_1804/user/0180-u-configure_default_applications/configure_default_applications.sh
+++ ubuntu_1804/user/0180-u-configure_default_applications/configure_default_applications.sh
@@ -19,42 +19,48 @@ LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE}|xargs readlink -f)"
 exit_if_not_bash
 exit_if_has_root_privileges
 
-configure_default_applications(){
-	cd ${RECIPE_DIR}
+configure_default_applications_with_xdg_mime(){
+	printf "Configuring default applications with xdg-mime ...\n"
 	
-	if [[ -f mimeapps.list ]]; then
-		rm -f mimeapps.list
-	fi
-	
-	if [ -f ~/.config/mimeapps.list ]; then
-		backup_file rename ~/.config/mimeapps.list
-	fi
-	
-	#
-	# We override a few .desktop files:
-	#
-	#   - Caja: to be started with a script assuring one instance per
-	#     workspace.
-	#
-	#   - GPicView: gpicview.desktop which is installed with the package 
-	#     gives "Image Viewer" as application name but we don't want to
-	#     see that, it's name is GPicView, point.
-	#
-	mkdir -p ~/.local/share/applications
-	cp *.desktop ~/.local/share/applications
-	
-	echo "Configuring mate-caja as default file browser ..."
+	printf "  - Caja should be default file browser\n"
 	xdg-mime default caja.desktop inode/directory
 	
-	echo "Configuring thunderbird as default mail client ..."
-	xdg-mime default thunderbird.desktop x-scheme-handler/mailto
+	printf "  - Claws Mail should be default mail client\n"
+	xdg-mime default sylpheed-claws.desktop x-scheme-handler/mailto
+	
+	printf "\n"
+}
+
+configure_default_applications_with_desktop_file_overridings(){
+	printf "Configuring default applications with desktop files overridings ...\n"
+	
+	mkdir -p "${HOME}/.local/share/applications"
+	
+	printf "  - Caja: should to be started with a script assuring one instance per workspace\n"
+	cp "${RECIPE_DIR}/caja.desktop" "${HOME}/.local/share/applications"
+	
+	printf "  - GPicView: gpicview.desktop which is installed with the package gives 'Image Viewer' as application name but we don't want to see that. The name should be 'GPicView'\n"
+	cp "${RECIPE_DIR}/gpicview.desktop" "${HOME}/.local/share/applications"
+	
+	printf "\n"
+}
+
+configure_default_applications_with_mime_apps_list(){
+	printf "Configuring default applications with mimeapp.list ...\n"
+	
+	# We will work on a temporary file
+	TEMP_MIME_APPS_LIST_FILE="${RECIPE_DIR}/mymimeapps.list"
+	if [[ -f "${TEMP_MIME_APPS_LIST_FILE}" ]]; then
+		rm -f "${TEMP_MIME_APPS_LIST_FILE}"
+	fi
 	
 	# MIME types are defined in /etc/mime.types
 	# Set default applications per MIME types:
-	#   text/*			Geany
-	#   image/* 		GPicView
-	#   audio/*			VLC
-	#   video/*			VLC
+	#  text/*		Geany
+	#  image/* 		GPicView
+	#  audio/*		VLC
+	#  video/*		VLC
+	# inode/directory	VLC
 	
 	while read LINE ; do
 		MIME_TYPE=""
@@ -68,58 +74,69 @@ configure_default_applications(){
 			continue
 		fi
 		if [[ "${MIME_TYPE}" =~ ^text/ ]]; then
-			echo "${MIME_TYPE}=geany.desktop" >>./mimeapps.list
+			echo "${MIME_TYPE}=geany.desktop" >>"${TEMP_MIME_APPS_LIST_FILE}"
 			continue
 		fi
 		if [[ "${MIME_TYPE}" =~ ^application/.*\+xml.* ]]; then
-			echo "${MIME_TYPE}=geany.desktop" >>./mimeapps.list
+			echo "${MIME_TYPE}=geany.desktop" >>"${TEMP_MIME_APPS_LIST_FILE}"
 			continue
 		fi
 		if [[ "${MIME_TYPE}" =~ ^application/xml-.* ]]; then
-			echo "${MIME_TYPE}=geany.desktop" >>./mimeapps.list
+			echo "${MIME_TYPE}=geany.desktop" >>"${TEMP_MIME_APPS_LIST_FILE}"
 			continue
 		fi
 		if [[ "${MIME_TYPE}" =~ ^image/ ]]; then
-			echo "${MIME_TYPE}=gpicview.desktop" >>./mimeapps.list
+			echo "${MIME_TYPE}=gpicview.desktop" >>"${TEMP_MIME_APPS_LIST_FILE}"
 			continue
 		fi
 		if [[ "${MIME_TYPE}" =~ ^audio/ ]]; then
-			echo "${MIME_TYPE}=vlc.desktop" >>./mimeapps.list
+			echo "${MIME_TYPE}=vlc.desktop" >>"${TEMP_MIME_APPS_LIST_FILE}"
 			continue
 		fi
 		if [[ "${MIME_TYPE}" =~ ^video/ ]]; then
-			echo "${MIME_TYPE}=vlc.desktop" >>./mimeapps.list
+			echo "${MIME_TYPE}=vlc.desktop" >>"${TEMP_MIME_APPS_LIST_FILE}"
 			continue
 		fi
 		if [[ "${MIME_TYPE}" == 'application/pdf' ]]; then
-			echo "${MIME_TYPE}=atril.desktop" >>./mimeapps.list
+			echo "${MIME_TYPE}=atril.desktop" >>"${TEMP_MIME_APPS_LIST_FILE}"
 			continue
 		fi
-		if [[ "${MIME_TYPE}" == 'inode/directory' ]]; then
-			echo "${MIME_TYPE}=vlc.desktop" >>./mimeapps.list
-			continue
-		fi
-		echo "${MIME_TYPE}=" >>./mimeapps.list
+		echo "${MIME_TYPE}=" >>"${TEMP_MIME_APPS_LIST_FILE}"
 	done < /etc/mime.types
 	
-	echo "application/xml=geany.desktop" >>./mimeapps.list
+	printf "application/xml=geany.desktop\n" >>"${TEMP_MIME_APPS_LIST_FILE}"
+	printf "inode/directory=vlc.desktop\n" >>"${TEMP_MIME_APPS_LIST_FILE}"
 	
-	DEFAULT_APPLICATIONS_LINE_NUMBER=$(grep -nr -E "^\[Default Applications]" ~/.config/mimeapps.list|awk -F ":" '{print $1}')
-	sed -i.bak "/^\[Default Applications\]$/ r ./mimeapps.list"  ~/.config/mimeapps.list
+	if [[ -f "${HOME}/.config/mimeapps.list" ]]; then
+		backup_file rename "${HOME}/.config/mimeapps.list"
+	fi
+	printf '[Default Applications]\n' >"${HOME}/.config/mimeapps.list"
+	cat "${TEMP_MIME_APPS_LIST_FILE}" >>"${HOME}/.config/mimeapps.list"
 	
-	# Here we have not modified the .desktop files in /usr => no need to call 'update-desktop-database'
+	# We have not modified the .desktop files in /usr => no need to call 'update-desktop-database'
 	# Calling 'update-desktop-database' with no arguments will use $XDG_DATA_DIRS which on Ubuntu18.04 is valued with:
 	# /usr/share/openbox:/usr/local/share/:/usr/share/
 	#update-desktop-database
 	
 	# Cleaning
-	cd ${RECIPE_DIR}
-	rm -f mimeapps.list
+	rm -f "${TEMP_MIME_APPS_LIST_FILE}"
 	
-	echo
+	printf "\n"
 }
 
-configure_default_applications 2>&1 | tee -a "${LOGFILE}"
+configure_default_applications_with_xdg_mime 2>&1 | tee -a "${LOGFILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
+
+configure_default_applications_with_desktop_file_overridings 2>&1 | tee -a "${LOGFILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
+
+configure_default_applications_with_mime_apps_list 2>&1 | tee -a "${LOGFILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
