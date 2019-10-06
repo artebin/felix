f110d9627ae06dda77a1d74dba099450f5650333
Nicolas James <nicolas.james@gmail.com>
2019-10-06 16:43:08 +0200

    enh recipe configure_gsimplecal

--- ubuntu_1804/user/0080-u-configure_gsimplecal/configure_gsimplecal.sh
+++ ubuntu_1804/user/0080-u-configure_gsimplecal/configure_gsimplecal.sh
@@ -13,18 +13,19 @@ exit_if_not_bash
 exit_if_has_root_privileges
 
 configure_gsimplecal(){
-	echo "Configuring gsimplecal ..."
-	if [[ -d "${HOME}/.config/gsimplecal" ]]; then
-		backup_file rename "${HOME}/.config/gsimplecal"
+	printf "Configuring gsimplecal ...\n"
+	
+	if [[ ! -d "${HOME}/.config/gsimplecal" ]]; then
+		mkdir -p "${HOME}/.config/gsimplecal"
 	fi
-	mkdir -p "${HOME}/.config/gsimplecal"
-	cp "${RECIPE_FAMILY_DIR}/dotfiles/.config/gsimplecal/config" "${HOME}/.config/gsimplecal/config"
+	backup_by_rename_if_exists_and_copy_replacement "${HOME}/.config/gsimplecal/config" "${RECIPE_FAMILY_DIR}/dotfiles/.config/gsimplecal/config"
 	SED_PATTERN="LOCAL_TIME_ZONE"
 	ESCAPED_SED_PATTERN=$(escape_sed_pattern ${SED_PATTERN})
 	REPLACEMENT_STRING="${LOCAL_TIME_ZONE}"
 	ESCAPED_REPLACEMENT_STRING=$(escape_sed_pattern ${REPLACEMENT_STRING})
 	sed -i "s/${ESCAPED_SED_PATTERN}/${ESCAPED_REPLACEMENT_STRING}/g" "${HOME}/.config/gsimplecal/config"
-	echo
+	
+	printf "\n"
 }
 
 configure_gsimplecal 2>&1 | tee -a "${LOGFILE}"
