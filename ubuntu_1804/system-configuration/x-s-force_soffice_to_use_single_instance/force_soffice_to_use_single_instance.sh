47aeafdd696f97a4457e0b2f07da4b390e5c31f1
Nicolas James <nicolas.james@gmail.com>
2019-10-06 15:19:29 +0200

    updated force_soffice_to_use_single_instance

--- ubuntu_1804/system-configuration/x-s-force_soffice_to_use_single_instance/force_soffice_to_use_single_instance.sh
+++ ubuntu_1804/system-configuration/x-s-force_soffice_to_use_single_instance/force_soffice_to_use_single_instance.sh
@@ -1,39 +1,28 @@
 #!/usr/bin/env bash
 
-RECIPE_DIR="$(dirname ${BASH_SOURCE}|xargs readlink -f)"
-FELIX_ROOT="${RECIPE_DIR%/felix/*}/felix"
+declare -g RECIPE_DIR="$(dirname ${BASH_SOURCE}|xargs readlink -f)"
+declare -g FELIX_ROOT="${RECIPE_DIR%/felix/*}/felix"
 if [[ ! -f "${FELIX_ROOT}/felix.sh" ]]; then
 	printf "Cannot find ${FELIX_ROOT}/felix.sh\n"
 	exit 1
 fi
 source "${FELIX_ROOT}/felix.sh"
-RECIPE_FAMILY_DIR=$(retrieve_recipe_family_dir "${RECIPE_DIR}")
-RECIPE_FAMILY_CONF_FILE=$(retrieve_recipe_family_conf_file "${RECIPE_DIR}")
-if [[ ! -f "${RECIPE_FAMILY_CONF_FILE}" ]]; then
-	printf "Cannot find RECIPE_FAMILY_CONF_FILE: ${RECIPE_FAMILY_CONF_FILE}\n"
-	exit 1
-fi
-source "${RECIPE_FAMILY_CONF_FILE}"
-LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE}|xargs readlink -f)"
+init_recipe "${RECIPE_DIR}"
+
+force_soffice_to_use_single_instance(){
+	printf "Force soffice to use single instance ...\n"
+	
+	cp soffice_single_instance.sh /usr/local/bin
+	chmod 755 /usr/local/bin/soffice_single_instance.sh
+	update-alternatives --install /usr/bin/soffice soffice /usr/local/bin/soffice_single_instance.sh 10
+	
+	printf "\n"
+}
 
 exit_if_not_bash
 exit_if_has_not_root_privileges
 
-force_geany_to_reuse_same_instance_per_workspace(){
-	echo "Force Geany to reuse same instance per workspace ..."
-	
-	# I cannot make it work by overriding geany.desktop in ${HOME}/.local/share/applications
-	# Here we fix /usr/share/applications/geany.desktop
-	
-	cp "${RECIPE_DIR}/geany_one_instance_per_workspace.sh" /usr/bin/geany_one_instance_per_workspace
-	chmod a+x /usr/bin/geany_one_instance_per_workspace
-	sed -i "s/Exec=.*/Exec=bash geany_one_instance_per_workspace %F/" /usr/share/applications/geany.desktop
-	update-desktop-database
-	
-	echo
-}
-
-force_geany_to_reuse_same_instance_per_workspace 2>&1 | tee -a "${LOGFILE}"
+force_soffice_to_use_single_instance 2>&1 | tee -a "${LOGFILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
