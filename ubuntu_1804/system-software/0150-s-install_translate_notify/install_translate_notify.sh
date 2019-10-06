0cd03128d2009df8015449e2b03f1d7bf6a4b31b
Nicolas James <nicolas.james@gmail.com>
2019-10-06 15:38:34 +0200

    system-software uses init_recipe

--- ubuntu_1804/system-software/0150-s-install_translate_notify/install_translate_notify.sh
+++ ubuntu_1804/system-software/0150-s-install_translate_notify/install_translate_notify.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-RECIPE_DIR="$(dirname ${BASH_SOURCE}|xargs readlink -f)"
-FELIX_ROOT="${RECIPE_DIR%/felix/*}/felix"
+declare -g RECIPE_DIR="$(dirname ${BASH_SOURCE}|xargs readlink -f)"
+declare -g FELIX_ROOT="${RECIPE_DIR%/felix/*}/felix"
 if [[ ! -f "${FELIX_ROOT}/felix.sh" ]]; then
 	printf "Cannot find ${FELIX_ROOT}/felix.sh\n"
 	exit 1
 fi
 source "${FELIX_ROOT}/felix.sh"
-RECIPE_FAMILY_DIR=$(retrieve_recipe_family_dir "${RECIPE_DIR}")
-RECIPE_FAMILY_CONF_FILE=$(retrieve_recipe_family_conf_file "${RECIPE_DIR}")
-if [[ ! -f "${RECIPE_FAMILY_CONF_FILE}" ]]; then
-	printf "Cannot find RECIPE_FAMILY_CONF_FILE: ${RECIPE_FAMILY_CONF_FILE}\n"
-	exit 1
-fi
-source "${RECIPE_FAMILY_CONF_FILE}"
-LOGFILE="$(retrieve_log_file_name ${BASH_SOURCE}|xargs readlink -f)"
+init_recipe "${RECIPE_DIR}"
 
 exit_if_not_bash
 exit_if_has_not_root_privileges
