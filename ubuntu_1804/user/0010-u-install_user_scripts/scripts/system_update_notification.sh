1b40924f7ab1266436e6d8aeddcf63d1ea2897e1
Nicolas James <nicolas.james@gmail.com>
2020-02-01 18:27:11 +0100

    minor improvement in system_update_notification.sh

--- ubuntu_1804/user/0010-u-install_user_scripts/scripts/system_update_notification.sh
+++ ubuntu_1804/user/0010-u-install_user_scripts/scripts/system_update_notification.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SYSTEM_UPDATE_NOTIFICATION_STATUS_FILE="/dev/shm/${USER}.system_update_notification.status"
+export SYSTEM_UPDATE_UI_STATUS_FILE="/dev/shm/${USER}.system_update_ui.status"
 export SECURITY_UPDATE_PACKAGE_LIST_FILE="/dev/shm/${USER}.system_update_package.list"
 
 SECURITY_UPDATE_PACKAGE_NAME_ARRAY=""
@@ -15,14 +15,28 @@ retrieve_security_updates(){
 
 # Handler to process yad shutdown
 function system_update_notification_exit() {
-	YAD_PID=$(cat "${SYSTEM_UPDATE_NOTIFICATION_STATUS_FILE}")
+	YAD_PID=$(cat "${SYSTEM_UPDATE_UI_STATUS_FILE}")
 	kill -9 "${YAD_PID}"
 }
 export -f system_update_notification_exit
 
 # Handler for tray icon left click
 function system_update_notification_click_left() {
-	yad --text-info < "${SECURITY_UPDATE_PACKAGE_LIST_FILE}"
+	yad --list < "${SECURITY_UPDATE_PACKAGE_LIST_FILE}" \
+	--column=Package \
+	--window-icon="software-update-urgent" \
+	--width=600 \
+	--height=500 \
+	--wrap \
+	--title="Security Updates Available" \
+	--button="Install Updates:0" \
+	--button="gtk-close:1" \
+	--on-top &
+	YAD_DIALOG_PID=$!
+	# There should be a way to register a callback to run when the YAD window is visible instead of the sleep below
+	sleep 0.2
+	YAD_DIALOG_WINDOW_ID=$(wmctrl -l | grep -i "Security Updates Available" | awk '{print $1}')
+	wmctrl -i -a ${YAD_DIALOG_WINDOW_ID}
 }
 export -f system_update_notification_click_left
 
@@ -41,7 +55,7 @@ start_yad_notification(){
 		--menu="Quit!bash -c system_update_notification_menu_item_quit" \
 		--no-middle &
 	YAD_PID=$!
-	echo "${YAD_PID}" >"${SYSTEM_UPDATE_NOTIFICATION_STATUS_FILE}"
+	echo "${YAD_PID}" >"${SYSTEM_UPDATE_UI_STATUS_FILE}"
 }
 
 retrieve_security_updates
