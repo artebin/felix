6db3589907c472f486d740f24c075d077c9f96e9
Nicolas James <nicolas.james@gmail.com>
2020-04-20 11:47:59 +0200

    added recipe configure screen

---
+++ ubuntu_1804/user/0230-u-configure_screen/configure_screen.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+declare -g RECIPE_DIR="$(dirname ${BASH_SOURCE}|xargs readlink -f)"
+declare -g FELIX_ROOT="${RECIPE_DIR%/felix/*}/felix"
+if [[ ! -f "${FELIX_ROOT}/felix.sh" ]]; then
+	printf "Cannot find ${FELIX_ROOT}/felix.sh\n"
+	exit 1
+fi
+source "${FELIX_ROOT}/felix.sh"
+init_recipe "${RECIPE_DIR}"
+
+exit_if_not_bash
+exit_if_has_root_privileges
+
+configure_screen(){
+	printf "Configuring screen ...\n"
+	
+	backup_by_rename_if_exists_and_copy_replacement "${HOME}/.screenrc" "${RECIPE_FAMILY_DIR}/dotfiles/.screenrc"
+	
+	printf "\n"
+}
+
+configure_screen 2>&1 | tee -a "${LOGFILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
