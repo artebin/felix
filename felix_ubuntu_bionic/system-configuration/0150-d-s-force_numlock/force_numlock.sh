a506f7a353f9f4ed02db4818adb1eb69d56f04ea
Nicolas James <nicolas.james@gmail.com>
2020-05-15 12:38:33 +0200

    Fix: recipe force_numlock could not be re-executed

--- felix_ubuntu_bionic/system-configuration/0150-d-s-force_numlock/force_numlock.sh
+++ felix_ubuntu_bionic/system-configuration/0150-d-s-force_numlock/force_numlock.sh
@@ -13,30 +13,24 @@ exit_if_not_bash
 exit_if_has_not_root_privileges
 
 force_numlock_tty(){
-	echo "Forcing numlock in tty ..."
+	printf "Forcing numlock in tty ...\n"
 	
-	if [[ -f /usr/bin/numlock ]]; then
-		echo "/usr/bin/numlock already exists"
-		return 1
-	fi
-	cp "${RECIPE_DIRECTORY}/numlock" /usr/bin/numlock
+	cd "${RECIPE_DIRECTORY}"
+	cp numlock /usr/bin/numlock
 	chmod a+x /usr/bin/numlock
 	
-	if [[ -f /etc/systemd/system/numlock_tty.service ]]; then
-		echo "/etc/systemd/system/numlock_tty.service already exists"
-		return 1
-	fi
-	cp "${RECIPE_DIRECTORY}/numlock_tty.service" /etc/systemd/system/numlock_tty.service
+	cd "${RECIPE_DIRECTORY}"
+	cp numlock_tty.service /etc/systemd/system/numlock_tty.service
 	systemctl daemon-reload
 	systemctl start numlock_tty.service
 	systemctl status numlock_tty.service
 	systemctl enable numlock_tty.service
 	
-	echo
+	printf "\n"
 }
 
 force_numlock_xorg(){
-	echo "Forcing numlock in X.org ..."
+	printf "Forcing numlock in X.org ...\n"
 	
 	install_package_if_not_installed "numlockx"
 	if [[ ! -f "/etc/default/numlockx" ]]; then
@@ -45,16 +39,13 @@ force_numlock_xorg(){
 	fi
 	add_or_update_keyvalue "/etc/default/numlockx" "NUMLOCK" "on"
 	
-	if [[ -f /etc/lightdm/lightdm.conf.d/60-numlockx.conf ]]; then
-		echo "/etc/lightdm/lightdm.conf.d/60-numlockx.conf already exists"
-		return 1
-	fi
 	if [[ ! -d /etc/lightdm/lightdm.conf.d ]]; then
 		mkdir /etc/lightdm/lightdm.conf.d
 	fi
-	cp "${RECIPE_DIRECTORY}/60-numlockx.conf" /etc/lightdm/lightdm.conf.d
+	cd "${RECIPE_DIRECTORY}"
+	cp 60-numlockx.conf /etc/lightdm/lightdm.conf.d
 	
-	echo
+	printf "\n"
 }
 
 force_numlock_tty 2>&1 | tee -a "${RECIPE_LOG_FILE}"
