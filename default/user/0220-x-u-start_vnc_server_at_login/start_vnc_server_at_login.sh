d9bd8be350a1dabce73061e5b9a49e63facea44a
Nicolas James <nicolas.james@gmail.com>
2020-05-20 16:37:26 +0200

    Enh: added recipe start_vnc_server_at_login

---
+++ default/user/0220-x-u-start_vnc_server_at_login/start_vnc_server_at_login.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+RECIPE_DIRECTORY="$(dirname ${BASH_SOURCE}|xargs readlink -f)"
+FELIX_ROOT="${RECIPE_DIRECTORY%/felix/*}/felix"
+if [[ ! -f "${FELIX_ROOT}/felix.sh" ]]; then
+	printf "Cannot find ${FELIX_ROOT}/felix.sh\n"
+	exit 1
+fi
+source "${FELIX_ROOT}/felix.sh"
+initialize_recipe "${RECIPE_DIRECTORY}"
+
+exit_if_not_bash
+exit_if_has_root_privileges
+
+start_vnc_server_at_login(){
+	printf "Start VNC server at login ...\n"
+	
+	printf "Set VNC password ...\n"
+	vncpasswd
+	
+	printf "Adding .desktop file for starting VNC server at login ...\n"
+	cd "${RECIPE_DIRECTORY}"
+	cp "SetScreenLayoutAndStartVNCServer.desktop" "${HOME}/.config/autostart"
+	
+	printf "\n"
+}
+
+start_vnc_server_at_login 2>&1 | tee -a "${RECIPE_LOG_FILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
