302162d26d51551717d0897c00bb0a23ae5d4654
Nicolas James <nicolas.james@gmail.com>
2020-05-22 20:09:04 +0200

    Fix: restored install_clipmenu_from_sources

--- default/system-software/0070-d-s-install_clipmenu_from_sources/install_clipmenu_from_sources.sh
+++ default/system-software/0070-d-s-install_clipmenu_from_sources/install_clipmenu_from_sources.sh
@@ -46,33 +46,13 @@ install_clipmenu_from_sources(){
 	printf "\n"
 }
 
-install_clipmenu_from_archived_sources(){
-	printf "Installing ClipMenu from sources ...\n"
-	
-	cd "${RECIPE_DIRECTORY}"
-	unzip clipmenu-develop-180506-0111.zip
-	cd "clipmenu-develop"
-	
-	printf "Applying patch on clipmenud for copy/paste in file managers ...\n"
-	patch < ../clipmenud_190204.patch
-	
-	cp clipdel /usr/bin
-	cp clipmenu /usr/bin
-	cp clipmenud /usr/bin
-	
-	# Cleaning
-	rm -fr "${RECIPE_DIRECTORY}/clipmenu-develop"
-	
-	printf "\n"
-}
-
 install_clipnotify_from_sources 2>&1 | tee -a "${RECIPE_LOG_FILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
 fi
 
-install_clipmenu_from_archived_sources 2>&1 | tee -a "${RECIPE_LOG_FILE}"
+install_clipmenu_from_sources 2>&1 | tee -a "${RECIPE_LOG_FILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
