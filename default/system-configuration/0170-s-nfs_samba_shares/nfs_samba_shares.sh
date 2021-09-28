9fce082476bbfaa558ab4edb3155a44fc6832f08
Nicolas James <nicolas.james@gmail.com>
2021-09-28 17:14:44 +0200

    fix nfs_samba_shares

--- default/system-configuration/0170-s-nfs_samba_shares/nfs_samba_shares.sh
+++ default/system-configuration/0170-s-nfs_samba_shares/nfs_samba_shares.sh
@@ -16,7 +16,7 @@ nfs_samba_shares(){
 	printf "nfs samba shares...\n"
 	
 	cd "${RECIPE_DIRECTORY}"
-	cat exports_commends >>/etc/exports
+	cat exports_comments >>/etc/exports
 	cat fstab_comments >>/etc/fstab
 	cat auto.master_comments >>/etc/auto.master
 	
