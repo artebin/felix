7a177398b9b2406becf44502e0042649b889f80b
Nicolas James <nicolas.james@gmail.com>
2022-01-24 11:00:56 +0100

    added ssh-agent.sh in .bashrc.d

---
+++ felix-debian11/recipes/dotfiles/.bashrc.d/ssh-agent.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#if [ -z "$SSH_AUTH_SOCK" ] ; then
+#	eval `ssh-agent -s`
+#	ssh-add
+#fi
+#trap 'test -n "$SSH_AUTH_SOCK" && eval `/usr/bin/ssh-agent -k`' 0
+
+if [ ! -S ~/.ssh/ssh_auth_sock ]; then
+	eval `ssh-agent`
+	ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+fi
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+ssh-add -l > /dev/null || ssh-add
+
