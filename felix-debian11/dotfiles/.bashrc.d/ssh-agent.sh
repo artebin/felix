a866f052245974f4a34348afd06ebcb37d3d134c
Nicolas James <njames@voltaire>
2022-03-02 12:10:09 +0100

    better fix ssh-agent.sh

--- felix-debian11/dotfiles/.bashrc.d/ssh-agent.sh
+++ felix-debian11/dotfiles/.bashrc.d/ssh-agent.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ ! -d "${HOME}/.ssh" ]]; then
-	exit 0
+if [[ -d "${HOME}/.ssh" ]]; then
+	HOME_LINK_SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
+	if [[ ! -S "${HOME_LINK_SSH_AUTH_SOCK}" ]]; then
+		eval `ssh-agent`
+		ln -sf "${SSH_AUTH_SOCK}" "${HOME_LINK_SSH_AUTH_SOCK}"
+	fi
+	export SSH_AUTH_SOCK="${HOME_LINK_SSH_AUTH_SOCK}"
+	ssh-add -l > /dev/null || ssh-add
 fi
-
-# Using <https://unix.stackexchange.com/a/217223/169557>
-if [[ ! -S ~/.ssh/ssh_auth_sock ]]; then
-	eval `ssh-agent`
-	ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-fi
-export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-ssh-add -l > /dev/null || ssh-add
 
