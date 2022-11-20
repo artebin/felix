7d247da4bac944878fd720a1b4a6deac54dbc557
Nicolas James <nicolas.james@gmail.com>
2022-11-20 16:29:14 +0100

    cleaned todos

--- felix-debian11/recipes/user/0040-u-install_user_scripts/scripts/toggle_cron_task_for_gpodder.sh
+++ felix-debian11/recipes/user/0040-u-install_user_scripts/scripts/toggle_cron_task_for_gpodder.sh
@@ -6,7 +6,7 @@ GREP_EXIT_CODE="${?}"
 if [[ "${GREP_EXIT_CODE}" != 0 ]]; then
 	printf "0 * * * * gpo update && gpo download\n" >>crontab.tmp
 else
-	sed -i '/gpo update && gpo download/d' crontab.tmp >/dev/null
+	sed -i '/gpo update && gpo download/d' crontab.tmp
 fi
 crontab crontab.tmp
 rm -f crontab.tmp
