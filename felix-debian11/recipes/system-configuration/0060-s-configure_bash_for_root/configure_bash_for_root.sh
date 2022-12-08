ae9a9b20df1ae952695b0031bc9435f701c0e564
Nicolas James <nicolas.james@gmail.com>
2022-12-08 15:59:42 +0100

    fix refactoring user-dotfiles

--- felix-debian11/recipes/system-configuration/0060-s-configure_bash_for_root/configure_bash_for_root.sh
+++ felix-debian11/recipes/system-configuration/0060-s-configure_bash_for_root/configure_bash_for_root.sh
@@ -21,7 +21,7 @@ configure_bash_for_root(){
 		backup_file rename /root/.bashrc
 	fi
 	
-	cp "${FELIX_ROOT}/dotfiles/.bashrc" /root/.bashrc
+	cp "${FELIX_ROOT}/user-dotfiles/.bashrc" /root/.bashrc
 	
 	printf "\n"
 }
