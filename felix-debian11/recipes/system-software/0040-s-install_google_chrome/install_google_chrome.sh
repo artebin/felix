5c811fb187eebfcdb6898744db5cea05fe723032
Nicolas James <nicolas.james@gmail.com>
2022-12-08 16:30:39 +0100

    fixed recipe for google chrome

--- felix-debian11/recipes/system-software/0040-s-install_google_chrome/install_google_chrome.sh
+++ felix-debian11/recipes/system-software/0040-s-install_google_chrome/install_google_chrome.sh
@@ -17,16 +17,16 @@ exit_if_has_not_root_privileges
 install_chrome(){
 	cd "${RECIPE_DIRECTORY}"
 	
-	echo "Installing Google Chrome from google.com"
-	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-	if [ -f /etc/apt/sources.list.d/google-chrome.list ]; then
+	printf "Installing Google Chrome from Google APT repository...\n"
+	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	if [[ -f /etc/apt/sources.list.d/google-chrome.list ]]; then
 		backup_file rename /etc/apt/sources.list.d/google-chrome.list
 	fi
-	echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+	printf "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main\n" > /etc/apt/sources.list.d/google-chrome.list
 	apt-get update
 	apt-get install google-chrome-stable -y
 	
-	echo
+	printf "\n"
 }
 
 
