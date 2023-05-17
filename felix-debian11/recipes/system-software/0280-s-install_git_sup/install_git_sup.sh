ab797102cf708019350a3a052979670b823535ef
Nicolas James <nicolas.james@gmail.com>
2023-05-17 17:25:45 +0200

    added install_git_sup

---
+++ felix-debian11/recipes/system-software/0280-s-install_git_sup/install_git_sup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+RECIPE_DIRECTORY="$(dirname ${BASH_SOURCE}|xargs readlink -f)"
+FELIX_SH="$(eval find ./$(printf "{$(echo %{1..10}q,)}" | sed 's/ /\.\.\//g')/ -maxdepth 1 -name felix.sh)"
+if [[ ! -f "${FELIX_SH}" ]]; then
+	printf "Cannot find felix.sh\n"
+	exit 1
+fi
+FELIX_SH="$(readlink -f "${FELIX_SH}")"
+FELIX_ROOT="$(dirname "${FELIX_SH}")"
+source "${FELIX_SH}"
+initialize_recipe "${RECIPE_DIRECTORY}"
+
+exit_if_not_bash
+exit_if_has_not_root_privileges
+
+install_git_sup(){
+	printf "Installing git_sup ...\n"
+	
+	git clone https://github.com/artebin/git_sup
+	cd git_sup
+	cp git_sup /usr/local/bin
+	rm -fr git_sup
+	
+	printf "\n"
+}
+
+cd "${RECIPE_DIRECTORY}"
+install_git_sup 2>&1 | tee -a "${RECIPE_LOG_FILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
