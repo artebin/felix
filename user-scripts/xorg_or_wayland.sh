892f9dcc26ca4e5129a1b1be6da25ad526005cbc
Nicolas James <nicolas.james@gmail.com>
2023-12-11 17:54:03 +0100

    added user script

---
+++ user-scripts/xorg_or_wayland.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+printf "XDG_SESSION_TYPE: %s\n" "${XDG_SESSION_TYPE}"
+printf "WAYLAND_DISPLAY: %s\n" "${WAYLAND_DISPLAY}"
+printf "DESKTOP_SESSION: %s\n" "${DESKTOP_SESSION}"
