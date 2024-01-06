d76d58bf4f1cc03d314779a1a8582ed61275abcf
Nicolas James <nicolas.james@gmail.com>
2024-01-06 13:20:18 +0100

    fixed fdpowermon_run.sh

--- user-scripts/fdpowermon_run.sh
+++ user-scripts/fdpowermon_run.sh
@@ -4,7 +4,7 @@
 RUNNING_IN_VIRTUAL_MACHINE=$(systemd-detect-virt)
 BATTERY_INFO=$(upower -e|grep 'BAT')
 
-if [[ -z "${RUNNING_IN_VIRTUAL_MACHINE}" ]] && [[ ! -z "${BATTERY_INFO}" ]]; then
+if [[ -z "${RUNNING_IN_VIRTUAL_MACHINE}" || "${RUNNING_IN_VIRTUAL_MACHINE,,}" == "none" ]] && [[ ! -z "${BATTERY_INFO}" ]]; then
 	fdpowermon &
 else
 	printf "fdpowermon_run.sh: not starting fdpowermon RUNNING_IN_VIRTUAL_MACHINE[%s] BATTERY_INFO[%s]\n" "${RUNNING_IN_VIRTUAL_MACHINE}" "${BATTERY_INFO}"
