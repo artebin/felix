8ca17a8c69185134e67076192f63ec54cd3259f7
Nicolas James <nicolas.james@gmail.com>
2024-01-08 20:20:12 +0100

    fixed ssh-agent

--- user-dotfiles/.xsessionrc.d/felix/09_ssh_agent.sh
+++ user-dotfiles/.xsessionrc.d/felix/09_ssh_agent.sh
@@ -4,6 +4,8 @@ if [ -d "${HOME}/.ssh" ]; then
 	HOME_LINK_SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
 	if [ ! -S "${HOME_LINK_SSH_AUTH_SOCK}" ]; then
 		eval `ssh-agent /usr/bin/dbus-launch --exit-with-session x-session-manager`
+		ln -sf "${SSH_AUTH_SOCK}" "${HOME_LINK_SSH_AUTH_SOCK}"
 	fi
-	ssh-add -l > /dev/null | ssh-add
+	export SSH_AUTH_SOCK="${HOME_LINK_SSH_AUTH_SOCK}"
+	ssh-add -l > /dev/null || ssh-add
 fi
