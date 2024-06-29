b9d29a0eb86edd5321a0d5c0144c6ab4cbd390cd
Nicolas James <nicolas.james@gmail.com>
2024-06-29 15:04:11 +0200

    fixed install_openbox optionally from sources with patches applied

--- felix-debian12/recipes/system-software/0031-s-install_openbox/install_openbox.sh
+++ felix-debian12/recipes/system-software/0031-s-install_openbox/install_openbox.sh
@@ -15,34 +15,42 @@ exit_if_not_bash
 exit_if_has_not_root_privileges
 
 function install_openbox(){
-	printf "Install caja...\n"
-	install_package_if_not_installed "openbox" "obconf"
+	printf "Install openbox...\n"
+	install_package_if_not_installed "openbox obconf"
 	printf "\n"
 }
 
-function install_openbox_from_sources(){
-	printf "Retrieve Debian sources for openbox package and install build dependencies...\n"
-	cd "${RECIPE_DIRECTORY}"
+function install_openbox_3_6_1_from_sources(){
+	printf "Retrieve sources for openbox package and install build dependencies...\n"
+	mkdir debian-openbox
+	cd "${RECIPE_DIRECTORY}"/debian-openbox
 	apt-get source openbox/stable
+	if [[ ! -d "openbox-3.6.1" ]]; then
+		printf "!ERROR! Cannot find openbox-3.6.1 directory\n"
+		exit 1
+	fi
 	sudo apt-get build-dep openbox/stable
 	
-	install_package_if_not_installed "devscripts"
+	printf "Apply patch for claiming the support of GTK_FRAME_EXTENTS from <https://github.com/jalopezg-git/openbox>...\n"
+	cd "${RECIPE_DIRECTORY}"/debian-openbox/openbox-3.6.1
+	patch -p1 < "${RECIPE_DIRECTORY}"/openbox-claim-gtk_frame_extents.patch
+	EDITOR=/bin/true dpkg-source -q --commit . gtk_frame_extents
 	
-	printf "Apply patch for GTK_FRAME_EXTENTS from <https://github.com/jalopezg-git/openbox>...\n"
-	cd "${RECIPE_DIRECTORY}"
-	GTK_FRAME_EXTENTS_COMMIT_SHA="d5a0e47025cf1a002f827740cc77d84f71d0d7aa"
-	curl https://github.com/jalopezg-git/openbox/commit/${GTK_FRAME_EXTENTS_COMMIT_SHA}.patch/ >${GTK_FRAME_EXTENTS_COMMIT_SHA}.patch
-	cd openbox-3.6.1
-	patch -p1 < ../${GTK_FRAME_EXTENTS_COMMIT_SHA}.patch
-	dpkg-source --commit
+	printf "Apply patch for ignoring size inc hints...\n"
+	cd "${RECIPE_DIRECTORY}"/debian-openbox/openbox-3.6.1
+	patch -p0 < "${RECIPE_DIRECTORY}"/openbox-ignore-size-inc-hints.patch
+	EDITOR=/bin/true dpkg-source -q --commit . ignore_size_inc_hints
+	
+	printf "Build and install openbox...\n"
+	cd "${RECIPE_DIRECTORY}"/debian-openbox/openbox-3.6.1
 	debuild -us -uc
 	
-	# then we can add the changes with $dpkg-source --commit
-	# then build the package with $debuild -us -uc
-	# then install the package
+	cd "${RECIPE_DIRECTORY}"/debian-openbox
+	dpkg -i libob* openbox_3.6.1-10_amd64.deb openbox-dbgsym_3.6.1-10_amd64.deb openbox-dev_3.6.1-10_amd64.deb
 	
-	# We do not build obconf from sources but install it from Debian repository
-	#install_package_if_not_installed "obconf"
+	# Cleanup
+	cd "${RECIPE_DIRECTORY}"
+	rm -fr debian-openbox
 	
 	printf "\n"
 }
@@ -56,7 +64,7 @@ if [[ "${FELIX_RECIPE_BUILD_FROM_SOURCES_ARRAY[${RECIPE_ID}]}" != "true" ]]; the
 		exit "${EXIT_CODE}"
 	fi
 else
-	install_openbox_from_sources 2>&1 | tee -a "${RECIPE_LOG_FILE}"
+	install_openbox_3_6_1_from_sources 2>&1 | tee -a "${RECIPE_LOG_FILE}"
 	EXIT_CODE="${PIPESTATUS[0]}"
 	if [[ "${EXIT_CODE}" -ne 0 ]]; then
 		exit "${EXIT_CODE}"
