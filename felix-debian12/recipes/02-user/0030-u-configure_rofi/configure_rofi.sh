18ae6d614b9b0f6419df731add025bc5bb887ac6
Nicolas James <nicolas.james@gmail.com>
2024-12-29 13:07:41 +0100

    add todo

--- felix-debian12/recipes/02-user/0030-u-configure_rofi/configure_rofi.sh
+++ felix-debian12/recipes/02-user/0030-u-configure_rofi/configure_rofi.sh
@@ -93,6 +93,7 @@ if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
 fi
 
+# TODO: delete the function below as we have another recipe installing rofimoji.
 #install_rofimoji 2>&1 | tee -a "${RECIPE_LOG_FILE}"
 #EXIT_CODE="${PIPESTATUS[0]}"
 #if [[ "${EXIT_CODE}" -ne 0 ]]; then
