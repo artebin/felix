7d0d827844a6df6a50f67ab719848bf89d3f5a85
Nicolas James <nicolas.james@gmail.com>
2025-06-08 16:39:44 +0200

    enh+fix gtk system configuration

--- felix-debian12/recipes/01-system/0390-s-configure_gtk/configure_gtk.sh
+++ felix-debian12/recipes/01-system/0390-s-configure_gtk/configure_gtk.sh
@@ -82,11 +82,52 @@ function configure_gtk3(){
 	mkdir -p /root/.config/gtk-3.0
 	cp "${FELIX_ROOT}/user-dotfiles/.config/gtk-3.0/gtk.css" /root/.config/gtk-3.0/gtk.css
 	
+	printf "\n"
+}
+
+function configure_gtk4(){
+	printf "Configuring GTK4 ...\n"
+	
+	if [[ -f /etc/gtk-4.0/settings.ini ]]; then
+		backup_file rename /etc/gtk-4.0/settings.ini
+	fi
+	
+	cp "${FELIX_ROOT}/user-dotfiles/.config/gtk-4.0/settings.ini" /etc/gtk-4.0/settings.ini
+	chmod 755 /etc/gtk-4.0/settings.ini
+	
+	if [[ -z "${GTK_CURSOR_THEME_NAME}" ]]; then
+		GTK_CURSOR_THEME_NAME="Adwaita"
+	fi
+	printf "Setting gtk-cursor-theme-name: ${GTK_CURSOR_THEME_NAME} ...\n"
+	sed -i "/^gtk-cursor-theme-name/s/.*/gtk-cursor-theme-name=${GTK_CURSOR_THEME_NAME}/" /etc/gtk-4.0/settings.ini
+	
+	if [[ -z "${GTK_THEME_NAME}" ]]; then
+		GTK_THEME_NAME="Adwaita"
+	fi
+	printf "Setting gtk-theme-name: ${GTK_THEME_NAME} ...\n"
+	sed -i "/^gtk-theme-name/s/.*/gtk-theme-name=${GTK_THEME_NAME}/" /etc/gtk-4.0/settings.ini
+	
+	if [[ -z "${GTK_ICON_THEME_NAME}" ]]; then
+		GTK_ICON_THEME_NAME="Adwaita"
+	fi
+	printf "Setting gtk-icon-theme-name: ${GTK_ICON_THEME_NAME} ...\n"
+	sed -i "/^gtk-icon-theme-name/s/.*/gtk-icon-theme-name=${GTK_ICON_THEME_NAME}/" /etc/gtk-4.0/settings.ini
+	
+	printf "Adding gtk.css for root ...\n"
+	if [[ -d /root/.config/gtk-4.0 ]]; then
+		backup_file rename /root/.config/gtk-4.0
+	fi
+	mkdir -p /root/.config/gtk-4.0
+	cp "${FELIX_ROOT}/user-dotfiles/.config/gtk-4.0/gtk.css" /root/.config/gtk-4.0/gtk.css
+	
+	printf "\n"
+}
+
+function add_gtk_environment_variables(){
+	printf "Configuring GTK environment variables ...\n"
+	
 	printf 'Adding environment variables for configuring GTK ...\n'
-	printf '  - Forcing GTK to use GDK backend x11\n'
-	printf "  - Disabling the default use of client-side decorations (CSD) on GTK windows\n"
 	printf "  - Disabling the scrollbar overlays introduced in GTK 3.16\n"
-	printf "  - Configuring GTK3 in SWT\n"
 	cp 99gtk.conf /etc/environment.d
 	
 	printf "\n"
@@ -99,6 +140,18 @@ if [[ "${EXIT_CODE}" -ne 0 ]]; then
 fi
 
 configure_gtk3 2>&1 | tee -a "${RECIPE_LOG_FILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
+
+configure_gtk4 2>&1 | tee -a "${RECIPE_LOG_FILE}"
+EXIT_CODE="${PIPESTATUS[0]}"
+if [[ "${EXIT_CODE}" -ne 0 ]]; then
+	exit "${EXIT_CODE}"
+fi
+
+add_gtk_environment_variables 2>&1 | tee -a "${RECIPE_LOG_FILE}"
 EXIT_CODE="${PIPESTATUS[0]}"
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
 	exit "${EXIT_CODE}"
