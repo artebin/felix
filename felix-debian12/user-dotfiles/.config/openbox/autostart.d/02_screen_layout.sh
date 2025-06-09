fe42065d654f609c70884b8b753355ca4651a9cf
Nicolas James <nicolas.james@gmail.com>
2025-06-09 16:34:16 +0200

    Fix screnlocker was not started anymore

--- felix-debian12/user-dotfiles/.config/openbox/autostart.d/02_screen_layout.sh
+++ felix-debian12/user-dotfiles/.config/openbox/autostart.d/02_screen_layout.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# Clear window layouts saved in a previous login
+# Remove all window layouts saved during a previous login
 rm -fr "${HOME}/.config/winlayout"
 
 if [ -f "${HOME}/.screenlayout/MyScreenLayout.sh" ];then
